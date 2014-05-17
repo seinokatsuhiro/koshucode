72f2c8130f8df8c1f700d93e9db41a9471b72a8a
SEINO Katsuhiro <seinokatsuhiro@me.com>
2014-05-17 16:18:25 +0900

    implement fill operator

--- baala/core/Koshucode/Baala/Core/Lexmap/Roamap.hs
+++ baala/core/Koshucode/Baala/Core/Lexmap/Roamap.hs
@@ -30,6 +30,8 @@ data RoamapBody
       -- ^ Add attribute.
     | RoamapRename  (String, String)
       -- ^ Rename attribute keyword.
+    | RoamapFill    [Maybe B.TokenTree]
+      -- ^ Fill positional attributes.
     | RoamapAppend  [Roamap]
       -- ^ Append roamaps.
       deriving (Show, Eq, Ord, G.Data, G.Typeable)
@@ -43,12 +45,17 @@ roamapCons = loop where
     notKeyword ('-' : _) = False
     notKeyword _         = True
 
+    fill (B.TreeL (B.TWord _ 0 "*") : xs) = Nothing : fill xs
+    fill (x : xs)                         = Just x  : fill xs
+    fill []                               = []
+
     right trees = Right . B.Sourced (B.untrees trees)
 
     loop trees =
-        B.abortableTrees "roamap" trees $ case B.divideTreesByBar trees of
-          [ B.TreeL (B.TWord _ 0 op) : _ ]
+        B.abortableTrees "attr" trees $ case B.divideTreesByBar trees of
+          [ B.TreeL (B.TWord _ 0 op) : xs ]
             | op == "id"        ->  right trees $ RoamapId
+            | op == "fill"      ->  right trees $ RoamapFill $ fill xs
 
           [ B.TreeL (B.TWord _ 0 op) : B.TreeL (B.TWord _ 0 k) : xs ]
             | notKeyword k      ->  Message.reqAttrName k
@@ -72,10 +79,13 @@ roamapCons = loop where
 roamapRun :: Roamap -> B.AbMap C.Roa
 roamapRun = loop where
     loop (B.Sourced toks rmap) roa =
-        B.abortable "roamap" toks $ case rmap of
-          RoamapId              ->  Right roa
-          RoamapAdd opt k xs    ->  add roa opt k xs
+        let Just pos = lookup "@trunk" roa
+        in B.abortable "attr" toks $ case rmap of
+          RoamapId              ->  Right  roa
+          RoamapAdd opt k xs    ->  add    roa opt k xs
           RoamapRename (k', k)  ->  rename roa k' k
+          RoamapFill xs         ->  do xs2 <- fill pos xs
+                                       Right $ ("@trunk", xs2) : roa
           RoamapAppend rs       ->  B.foldM (flip loop) roa rs
 
     add roa opt k xs =
@@ -89,4 +99,11 @@ roamapRun = loop where
         case lookup k roa of
           Just _              ->  Right $ B.assocRename1 k' k roa
           Nothing             ->  Message.reqAttr k
+
+    fill :: [B.TokenTree] -> [Maybe B.TokenTree] -> B.Ab [B.TokenTree]
+    fill (p : ps) (Nothing : xs)  =  Right . (p:) =<< fill ps xs
+    fill (p : ps) (_       : xs)  =  Right . (p:) =<< fill ps xs
+    fill []       (Just x  : xs)  =  Right . (x:) =<< fill [] xs
+    fill []       (Nothing : _ )  =  Message.reqAttr "*"
+    fill ps       []              =  Right $ ps
 
