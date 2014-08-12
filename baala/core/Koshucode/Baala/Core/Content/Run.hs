92f18b62ac0b595ef019ba5b0b18d5d1adab9e21
SEINO Katsuhiro <seinokatsuhiro@me.com>
2014-08-12 12:34:06 +0900

    remove show and write for beta expressions

--- baala/core/Koshucode/Baala/Core/Content/Run.hs
+++ baala/core/Koshucode/Baala/Core/Content/Run.hs
@@ -14,40 +14,25 @@ import qualified Koshucode.Baala.Core.Content.Class     as C
 import qualified Koshucode.Baala.Core.Content.Cox       as C
 import qualified Koshucode.Baala.Core.Message           as Message
 
+
+-- --------------------------------------------  Beta reduction
+
 data Beta c
-    = BetaLit    [B.CodePoint] c                   -- ^ Literal content
-    | BetaTerm   [B.CodePoint] [B.TermName] [Int]  -- ^ Term reference, its name and position
-    | BetaApplyL [B.CodePoint] String (C.CopFun c) [Beta c]  -- ^ Function application
+    = BetaLit  [B.CodePoint] c                     -- ^ Literal content
+    | BetaTerm [B.CodePoint] [B.TermName] [Int]    -- ^ Term reference, its name and position
+    | BetaCall [B.CodePoint] String (C.CopFun c) [Beta c]  -- ^ Function application
 
 instance B.CodePointer (Beta c) where
-    codePoint (BetaLit    cp _)      =  cp
-    codePoint (BetaTerm   cp _ _)    =  cp
-    codePoint (BetaApplyL cp _ _ _)  =  cp
-
-instance (B.Write c) => Show (Beta c) where
-    show = show . B.doc
-
-instance (B.Write c) => B.Write (Beta c) where
-    write = docBeta
-
-docBeta :: (B.Write c) => B.StringMap -> Beta c -> B.Doc
-docBeta sh = d (0 :: Int) where
-    d 10 _ = B.write sh "..."
-    d n e =
-        case e of
-          BetaLit    _ c      -> B.write  sh "lit" B.<+> B.write sh c
-          BetaTerm   _ ns _   -> B.writeH sh $ map ('/':) ns
-          BetaApplyL _ f _ xs -> let f'  = B.write sh "ap" B.<+> B.write sh f
-                                     xs' = B.nest 3 $ B.writeV sh (map d' xs)
-                                 in f' B.$$ xs'
-        where d' = d $ n + 1
+    codePoint (BetaLit  cp _)      =  cp
+    codePoint (BetaTerm cp _ _)    =  cp
+    codePoint (BetaCall cp _ _ _)  =  cp
 
 -- | Reduce content expression.
 beta :: (B.Write c) => C.CopBundle c -> B.Relhead -> C.Cox c -> B.Ab (Beta c)
-beta cops h =
-    reduce          -- beta reduction
-      . link cops   -- substitute free variables
-      B.<=< position h
+beta cops he =
+    reduce               -- beta reduction
+      . link cops        -- substitute free variables
+      B.<=< position he  -- put term index
 
 -- beta reduction, i.e., process CoxVar and CoxDeriv.
 reduce :: forall c. (B.Write c) => C.Cox c -> B.Ab (Beta c)
@@ -60,28 +45,29 @@ reduce = red [] where
         C.CoxLit    cp c       ->  Right $ BetaLit  cp c
         C.CoxTerm   cp n i     ->  Right $ BetaTerm cp n i
         C.CoxVar    cp v k     ->  ab1 cp $ red args =<< kth v k args
-        C.CoxApplyL cp fn xs   ->  ab1 cp $ do
-                                     xs' <- fill args `mapM` xs
-                                     join args fn xs'
+        C.CoxApplyL cp fn xs   ->  ab1 cp $ join args fn $ fills args xs
         C.CoxBase   cp _ _     ->  ab1 cp $ join args cox []
         C.CoxDeriv  cp _ v _   ->  ab1 cp $ Message.lackArg v
         C.CoxDerivL cp _ _ _   ->  ab1 cp $ Message.adlib "CoxDerivL"
 
-    join :: [B.Named (C.Cox c)] -> C.Cox c -> [C.Cox c] -> B.Ab (Beta c)
+    join :: [B.Named (C.Cox c)] -> C.Cox c -> [B.Ab (C.Cox c)] -> B.Ab (Beta c)
     join args fn []             =  red args fn
     join args fn xxs@(x:xs)     =  case fn of
-        C.CoxDeriv cp _ v f2   ->  ab2 cp $ do join ((v,x) : args) f2 xs
+        C.CoxDeriv cp _ v f2   ->  ab2 cp $ do
+                                       x' <- x
+                                       join ((v, x') : args) f2 xs
         C.CoxVar   cp v k      ->  ab2 cp $ do
-                                      fn'  <- kth v k args
-                                      xxs' <- fill args `mapM` xxs
-                                      join args fn' xxs'
+                                       fn' <- kth v k args
+                                       join args fn' xxs
         C.CoxBase  cp n f      ->  ab2 cp $ do
-                                      xxs' <- red args `mapM` xxs
-                                      Right $ BetaApplyL cp n f xxs'
-        C.CoxApplyL cp f2 xs2  ->  ab2 cp $ do
-                                      xs2' <- fill args `mapM` xs2
-                                      join args f2 $ xs2' ++ xxs
+                                       xxs2 <- mapM id xxs
+                                       xxs3 <- red args `mapM` xxs2
+                                       Right $ BetaCall cp n f xxs3
+        C.CoxApplyL cp f2 xs2  ->  ab2 cp $ join args f2 $ fills args xs2 ++ xxs
         _                      ->  Message.unkShow fn
+
+    fills :: [B.Named (C.Cox c)] -> [C.Cox c] -> [B.Ab (C.Cox c)]
+    fills = map . fill
 
     fill :: [B.Named (C.Cox c)] -> B.AbMap (C.Cox c)
     fill args (C.CoxVar _ v k)       = kth v k args
@@ -131,19 +117,7 @@ position he = spos where
     pos e = Right e
 
 
--- ----------------------  Run
-
-getArg1 :: [B.Ab c] -> B.Ab (B.Ab c)
-getArg1 [x]       = Right x
-getArg1 _         = Message.unmatchType ""
-
-getArg2 :: [B.Ab c] -> B.Ab (B.Ab c, B.Ab c)
-getArg2 [x, y]    = Right (x, y)
-getArg2 _         = Message.unmatchType ""
-
-getArg3 :: [B.Ab c] -> B.Ab (B.Ab c, B.Ab c, B.Ab c)
-getArg3 [x, y, z] = Right (x, y, z)
-getArg3 _         = Message.unmatchType ""
+-- --------------------------------------------  Run
 
 type RunCox  c = C.Cox c -> B.Ab c
 type RunList c = [c]     -> B.Ab c
@@ -168,10 +142,10 @@ coxRun args = run 0 where
     run lv cox =
         let run' = run $ lv + 1
         in B.abortable "cox-calc" [cox] $ case cox of
-             BetaLit    _ c       ->  Right c
-             BetaTerm   _ _ [p]   ->  Right $ args !!! p
-             BetaTerm   _ _ ps    ->  term ps args
-             BetaApplyL _ _ f xs  ->  f $ map run' xs
+             BetaLit  _ c       ->  Right c
+             BetaTerm _ _ [p]   ->  Right $ args !!! p
+             BetaTerm _ _ ps    ->  term ps args
+             BetaCall _ _ f xs  ->  f $ map run' xs
 
     term :: [Int] -> [c] -> B.Ab c
     term []       _ = Message.notFound "empty term"
@@ -192,4 +166,19 @@ list !!! index = loop index list where
     loop i (_ : xs) = loop (i - 1) xs
     loop _ _        = error $ show (B.doch list)
                               ++ " !!! " ++ show index
+
+
+-- --------------------------------------------  getArgN
+
+getArg1 :: [B.Ab c] -> B.Ab (B.Ab c)
+getArg1 [x]       = Right x
+getArg1 _         = Message.unmatchType ""
+
+getArg2 :: [B.Ab c] -> B.Ab (B.Ab c, B.Ab c)
+getArg2 [x, y]    = Right (x, y)
+getArg2 _         = Message.unmatchType ""
+
+getArg3 :: [B.Ab c] -> B.Ab (B.Ab c, B.Ab c, B.Ab c)
+getArg3 [x, y, z] = Right (x, y, z)
+getArg3 _         = Message.unmatchType ""
 
