4c03e22d439222ce67c2a65aef175cff354926a9
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-02-01 07:53:31 +0900

    modify comments

--- baala/core/Koshucode/Baala/Core/Relmap/Rop.hs
+++ baala/core/Koshucode/Baala/Core/Relmap/Rop.hs
@@ -1,25 +1,22 @@
 {-# OPTIONS_GHC -Wall #-}
 
--- | Implementation of relmap operators.
+-- | Relmap operators.
 
 module Koshucode.Baala.Core.Relmap.Rop
-  ( Rop' (..),
-    RopUsage,
-    RopUse' (..),
-    RopCons',
+  ( Rop' (..), RopUsage, RopCons', RopUse' (..),
   ) where
 
 import qualified Koshucode.Baala.Base                  as B
 import qualified Koshucode.Baala.Core.Lexmap           as C
 import qualified Koshucode.Baala.Core.Relmap.Relmap    as C
 
--- | Implementation of relmap operator
+-- | Implementation of relmap operator.
 data Rop' h c = Rop
     { ropName     :: C.RopName      -- ^ Operator name
     , ropGroup    :: String         -- ^ Operator group
     , ropSorter   :: C.AttrSort     -- ^ Attribute sorter
     , ropCons     :: RopCons' h c   -- ^ Constructor of operator
-    , ropUsage    :: RopUsage       -- ^ Usage of operator
+    , ropUsage    :: RopUsage       -- ^ Usage text of operator
     }
 
 instance Show (Rop' h c) where
@@ -29,10 +26,14 @@ instance Show (Rop' h c) where
 instance B.Name (Rop' h c) where
     name = ropName
 
--- | Operator usage.
+-- | Usage text of operator.
 type RopUsage = String
 
--- | Use of relmap operator
+-- | Constructor of relmap operator,
+--   i.e., function from use of operator to generic relmap.
+type RopCons' h c = RopUse' h c -> B.Ab (C.Relmap' h c)
+
+-- | Use of relmap operator.
 data RopUse' h c = RopUse
     { ropHook       :: h c
     , ropLexmap     :: C.Lexmap         -- ^ Syntactic data of operator use
@@ -41,7 +42,4 @@ data RopUse' h c = RopUse
 
 instance B.CodePtr (RopUse' h c) where
     codePtList = B.codePtList . ropLexmap
-
--- | Constructor of relmap operator
-type RopCons' h c = RopUse' h c -> B.Ab (C.Relmap' h c)
 
