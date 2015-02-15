c4dd6c066dd9b6f4058cc949e2b6ca3625be96db
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-02-15 14:24:43 +0900

    add backward newline conversion

--- baala/toolkit/crlf.sh
+++ baala/toolkit/crlf.sh
@@ -5,40 +5,71 @@ crlf_help () {
     echo "  Convert newlines from LF to CRLF"
     echo
     echo "USAGE"
-    echo "  $crlf_prog [-hlnru] FILE ..."
+    echo "  $crlf_prog [OPTIONS] FILE ..."
     echo
-    echo "OPTION"
+    echo "OPTIONS"
     echo "  -l   List files with symbols LF or CRLF"
     echo "  -n   List files only whose newline is LF (\\\n)"
     echo "  -r   List files only whose newline is CRLF (\\\r\\\n)"
-    echo "  -u   Update file by converting LF to CRLF"
+    echo "  -c   Change file by converting LF to CRLF"
+    echo "  -b   Change file backward, that is CRLF to LF"
     echo "  -h   Print this message"
+    echo
+    echo "EXAMPLES"
+    echo "  $crlf_prog *.txt               # list newline is LF or CRLF"
+    echo "  $crlf_prog -c *.txt            # convert LF to CRLF"
+    echo "  find . -name '*.txt' \\"
+    echo "    | xargs -n 10 $crlf_prog     # list all files named '*.txt'"
     echo
 }
 
 crlf_abort () {
     echo
     echo "ABORTED"
-    return 2
+    exit 2
 }
 
-crlf_not_contain () {
-    perl -ne 'exit 1 if /\r\n/' "$1"
-    status=$?
+# --------------------------------------------  List
 
-    case $status in
-        0) return 0   ;;
-        1) return 1   ;;
+crlf_contain () {
+    perl -ne 'exit 1 if /\r\n/' "$1"
+
+    case $? in
+        0) return 1   ;;
+        1) return 0   ;;
         *) crlf_abort ;;
     esac
 }
 
-crlf_add () {
-    perl -i -pe 's/\n/\r\n/' "$1"
+crlf_list_all () {
+    if crlf_contain "$1"; then
+        # newline is CRLF
+        echo "CRLF  $1"
+    else
+        # newline is LF, or file has no newlines
+        echo "LF    $1"
+    fi
 }
 
+crlf_list_crlf () {
+    if crlf_contain "$1"; then
+        echo "$1"
+    fi
+}
+
+crlf_list_lf () {
+    if ! crlf_contain "$1"; then
+        echo "$1"
+    fi
+}
+
+# --------------------------------------------  Change
+
+crlf_add    () { perl -i -pe 's/\n/\r\n/' "$1" ; }
+crlf_delete () { perl -i -pe 's/\r\n/\n/' "$1" ; }
+
 crlf_add_necessary () {
-    if crlf_not_contain "$1"; then
+    if ! crlf_contain "$1"; then
         crlf_add "$1"
         if [ $? = 0 ]; then
             echo "Convert LF to CRLF : $1"
@@ -48,40 +79,30 @@ crlf_add_necessary () {
     fi
 }
 
-crlf_list_all () {
-    if crlf_not_contain "$1"; then
-        # newlne is LF
-        # file that has no newline, especially empty file
-        echo "LF    $1"
-    else
-        # newlne is CRLF
-        echo "CRLF  $1"
+crlf_delete_necessary () {
+    if crlf_contain "$1"; then
+        crlf_delete "$1"
+        if [ $? = 0 ]; then
+            echo "Convert CRLF to LF : $1"
+        else
+            crlf_abort
+        fi
     fi
 }
 
-crlf_list_lf () {
-    if crlf_not_contain "$1"; then
-        echo "$1"
-    fi
-}
-
-crlf_list_crlf () {
-    if crlf_not_contain "$1"; then
-        :
-    else
-        echo "$1"
-    fi
-}
+# --------------------------------------------  Main
 
 crlf_prog=`basename $0`
 crlf_mode=list
 
-while getopts hnru crlf_opt; do
+while getopts bchlnr crlf_opt; do
     case $crlf_opt in
-        n) crlf_mode=lf     ;;
-        r) crlf_mode=crlf   ;;
-        u) crlf_mode=update ;;
-        *) crlf_help        ;;
+        b) crlf_mode=delete  ;;
+        c) crlf_mode=add     ;;
+        l) crlf_mode=list    ;;
+        n) crlf_mode=lf      ;;
+        r) crlf_mode=crlf    ;;
+        *) crlf_help         ;;
     esac
 done
 
@@ -89,10 +110,11 @@ shift $(($OPTIND - 1))
 
 for f in "$@"; do
     case $crlf_mode in
-        list)    crlf_list_all      "$f" ;;
-        lf)      crlf_list_lf       "$f" ;;
-        crlf)    crlf_list_crlf     "$f" ;;
-        update)  crlf_add_necessary "$f" ;;
-        *)       crlf_abort ;;
+        list)     crlf_list_all         "$f" ;;
+        lf)       crlf_list_lf          "$f" ;;
+        crlf)     crlf_list_crlf        "$f" ;;
+        add)      crlf_add_necessary    "$f" ;;
+        delete)   crlf_delete_necessary "$f" ;;
+        *)        crlf_abort                 ;;
     esac
 done
