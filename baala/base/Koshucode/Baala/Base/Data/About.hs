4c8f3ae077186b6138b6560d856f8390b0305083
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-06-30 04:09:15 +0900

    writer for about clause

--- baala/base/Koshucode/Baala/Base/Data/About.hs
+++ baala/base/Koshucode/Baala/Base/Data/About.hs
@@ -3,11 +3,16 @@
 module Koshucode.Baala.Base.Data.About
   ( About (..),
     AboutJudges,
+    writeDownAbout,
   ) where
 
 import qualified Koshucode.Baala.Base.Prelude      as B
+import qualified Koshucode.Baala.Base.Text         as B
 import qualified Koshucode.Baala.Base.Data.Judge   as B
 
 data About c = About [B.Named c] deriving (Show)
 type AboutJudges c = (Maybe (About c), [B.Judge c])
+
+writeDownAbout :: (B.Write c) => B.StringMap -> About c -> String
+writeDownAbout sh (About xs) = "about" ++ B.writeDownTerms sh xs
 
