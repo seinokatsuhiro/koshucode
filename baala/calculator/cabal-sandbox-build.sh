033cfb79ce9ee4e86bbe564cd1e3ebccd28765f1
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-09-10 05:44:00 +0900

    simple build script for koshu calculator

---
+++ baala/calculator/cabal-sandbox-build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+#  Build 'koshu' inside sandbox
+#
+
+check () {
+    if [ "$1" = 0 ]; then
+        shift
+        echo "ok - $*"
+    else
+        echo "ABORT - $*"
+        exit 2
+    fi
+}
+
+cabal sandbox init
+  check $? init
+
+cabal sandbox add-source ../base
+  check $? base
+cabal sandbox add-source ../core
+  check $? core
+cabal sandbox add-source ../writer
+  check $? writer
+cabal sandbox add-source ../operator
+  check $? operator
+cabal sandbox add-source ../content
+  check $? content
+
+cabal install --only-dependencies
+  check $? install --only-dependencies
+cabal build
+  check $? build
