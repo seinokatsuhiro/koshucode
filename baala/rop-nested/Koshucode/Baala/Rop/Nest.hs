7f17eeaefd3c64203c421cb4456e3e8853bb874f
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-10-17 14:33:07 +0900

    create top-nested package

---
+++ baala/rop-nested/Koshucode/Baala/Rop/Nest.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Relmap operators for nested relations.
+
+module Koshucode.Baala.Rop.Nest
+  ( module Koshucode.Baala.Rop.Nest.Confl,
+    module Koshucode.Baala.Rop.Nest.Deriv,
+    module Koshucode.Baala.Rop.Nest.Flow,
+    module Koshucode.Baala.Rop.Nest.Rop,
+  ) where
+
+import Koshucode.Baala.Rop.Nest.Confl
+import Koshucode.Baala.Rop.Nest.Deriv
+import Koshucode.Baala.Rop.Nest.Flow
+import Koshucode.Baala.Rop.Nest.Rop
+
