487fffde4ecc9e156c0bd9f9e62a6b3bba79020c
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-04-15 14:25:56 +0900

    functions for unicode minor categories

--- baala/base/Koshucode/Baala/Base/Text/Unicode.hs
+++ baala/base/Koshucode/Baala/Base/Text/Unicode.hs
@@ -4,9 +4,11 @@
 
 module Koshucode.Baala.Base.Text.Unicode
   ( GeneralCategoryGroup (..),
+    generalCategoryGroup,
     generalCategoryName,
     generalCategoryLetter,
-    generalCategoryGroup,
+    generalCategoryMajorLetter,
+    generalCategoryMinorLetter,
   ) where
 
 import qualified Data.Char as Ch
@@ -22,6 +24,52 @@ data GeneralCategoryGroup
     | UnicodeOther           -- ^ Cc Cf Cs Co Cn
       deriving (Show, Eq, Ord, Bounded)
 
+-- | Major general category of character.
+generalCategoryGroup :: Char -> GeneralCategoryGroup
+generalCategoryGroup = toMajorCategory . Ch.generalCategory
+
+-- | Convert general category to major category.
+toMajorCategory :: Ch.GeneralCategory -> GeneralCategoryGroup
+toMajorCategory Ch.UppercaseLetter        = UnicodeLetter
+toMajorCategory Ch.LowercaseLetter        = UnicodeLetter
+toMajorCategory Ch.TitlecaseLetter        = UnicodeLetter
+toMajorCategory Ch.ModifierLetter         = UnicodeLetter
+toMajorCategory Ch.OtherLetter            = UnicodeLetter
+
+toMajorCategory Ch.NonSpacingMark         = UnicodeMark
+toMajorCategory Ch.SpacingCombiningMark   = UnicodeMark
+toMajorCategory Ch.EnclosingMark          = UnicodeMark
+
+toMajorCategory Ch.DecimalNumber          = UnicodeNumber
+toMajorCategory Ch.LetterNumber           = UnicodeNumber
+toMajorCategory Ch.OtherNumber            = UnicodeNumber
+
+toMajorCategory Ch.ConnectorPunctuation   = UnicodePunctuation
+toMajorCategory Ch.DashPunctuation        = UnicodePunctuation
+toMajorCategory Ch.OpenPunctuation        = UnicodePunctuation
+toMajorCategory Ch.ClosePunctuation       = UnicodePunctuation
+toMajorCategory Ch.InitialQuote           = UnicodePunctuation
+toMajorCategory Ch.FinalQuote             = UnicodePunctuation
+toMajorCategory Ch.OtherPunctuation       = UnicodePunctuation
+
+toMajorCategory Ch.MathSymbol             = UnicodeSymbol
+toMajorCategory Ch.CurrencySymbol         = UnicodeSymbol
+toMajorCategory Ch.ModifierSymbol         = UnicodeSymbol
+toMajorCategory Ch.OtherSymbol            = UnicodeSymbol
+
+toMajorCategory Ch.Space                  = UnicodeSeperator
+toMajorCategory Ch.LineSeparator          = UnicodeSeperator
+toMajorCategory Ch.ParagraphSeparator     = UnicodeSeperator
+
+toMajorCategory Ch.Control                = UnicodeOther
+toMajorCategory Ch.Format                 = UnicodeOther
+toMajorCategory Ch.Surrogate              = UnicodeOther
+toMajorCategory Ch.PrivateUse             = UnicodeOther
+toMajorCategory Ch.NotAssigned            = UnicodeOther
+
+
+-- --------------------------------------------  Name
+
 -- | Name of general category.
 --   This function returns one of @letter@, @mark@,
 --   @number@, @punct@, @symbol@, @sep@, or @other@.
@@ -34,57 +82,61 @@ generalCategoryName UnicodeSymbol         = "symbol"
 generalCategoryName UnicodeSeperator      = "sep"
 generalCategoryName UnicodeOther          = "other"
 
+-- | Major-minor category symbols like @Lu@.
+generalCategoryLetter :: Ch.GeneralCategory -> String
+generalCategoryLetter cat =
+    [ generalCategoryMajorLetter $ toMajorCategory cat
+    , generalCategoryMinorLetter cat]
+
 -- | One-letter representation of general category.
 --   This function returns one of @L@ (letter),
 --   @M@ (mark), @N@ (number), @P@ (punctuation),
 --   @S@ (symbol), @Z@ (seperator), or @C@ (other).
-generalCategoryLetter :: GeneralCategoryGroup -> Char
-generalCategoryLetter UnicodeLetter       = 'L'
-generalCategoryLetter UnicodeMark         = 'M'
-generalCategoryLetter UnicodeNumber       = 'N'
-generalCategoryLetter UnicodePunctuation  = 'P'
-generalCategoryLetter UnicodeSymbol       = 'S'
-generalCategoryLetter UnicodeSeperator    = 'Z'
-generalCategoryLetter UnicodeOther        = 'C'
+generalCategoryMajorLetter :: GeneralCategoryGroup -> Char
+generalCategoryMajorLetter UnicodeLetter             = 'L'
+generalCategoryMajorLetter UnicodeMark               = 'M'
+generalCategoryMajorLetter UnicodeNumber             = 'N'
+generalCategoryMajorLetter UnicodePunctuation        = 'P'
+generalCategoryMajorLetter UnicodeSymbol             = 'S'
+generalCategoryMajorLetter UnicodeSeperator          = 'Z'
+generalCategoryMajorLetter UnicodeOther              = 'C'
 
--- | Major general category of character.
-generalCategoryGroup :: Char -> GeneralCategoryGroup
-generalCategoryGroup c =
-    case Ch.generalCategory c of
-      Ch.UppercaseLetter        -> UnicodeLetter
-      Ch.LowercaseLetter        -> UnicodeLetter
-      Ch.TitlecaseLetter        -> UnicodeLetter
-      Ch.ModifierLetter         -> UnicodeLetter
-      Ch.OtherLetter            -> UnicodeLetter
+-- | One-letter representation of minor general category.
+generalCategoryMinorLetter :: Ch.GeneralCategory -> Char
+generalCategoryMinorLetter Ch.UppercaseLetter        = 'u'
+generalCategoryMinorLetter Ch.LowercaseLetter        = 'l'
+generalCategoryMinorLetter Ch.TitlecaseLetter        = 't'
+generalCategoryMinorLetter Ch.ModifierLetter         = 'm'
+generalCategoryMinorLetter Ch.OtherLetter            = 'o'
 
-      Ch.NonSpacingMark         -> UnicodeMark
-      Ch.SpacingCombiningMark   -> UnicodeMark
-      Ch.EnclosingMark          -> UnicodeMark
+generalCategoryMinorLetter Ch.NonSpacingMark         = 'n'
+generalCategoryMinorLetter Ch.SpacingCombiningMark   = 's'
+generalCategoryMinorLetter Ch.EnclosingMark          = 'e'
 
-      Ch.DecimalNumber          -> UnicodeNumber
-      Ch.LetterNumber           -> UnicodeNumber
-      Ch.OtherNumber            -> UnicodeNumber
+generalCategoryMinorLetter Ch.DecimalNumber          = 'd'
+generalCategoryMinorLetter Ch.LetterNumber           = 'l'
+generalCategoryMinorLetter Ch.OtherNumber            = 'o'
 
-      Ch.ConnectorPunctuation   -> UnicodePunctuation
-      Ch.DashPunctuation        -> UnicodePunctuation
-      Ch.OpenPunctuation        -> UnicodePunctuation
-      Ch.ClosePunctuation       -> UnicodePunctuation
-      Ch.InitialQuote           -> UnicodePunctuation
-      Ch.FinalQuote             -> UnicodePunctuation
-      Ch.OtherPunctuation       -> UnicodePunctuation
+generalCategoryMinorLetter Ch.ConnectorPunctuation   = 'c'
+generalCategoryMinorLetter Ch.DashPunctuation        = 'd'
+generalCategoryMinorLetter Ch.OpenPunctuation        = 's'
+generalCategoryMinorLetter Ch.ClosePunctuation       = 'e'
+generalCategoryMinorLetter Ch.InitialQuote           = 'i'
+generalCategoryMinorLetter Ch.FinalQuote             = 'f'
+generalCategoryMinorLetter Ch.OtherPunctuation       = 'o'
 
-      Ch.MathSymbol             -> UnicodeSymbol
-      Ch.CurrencySymbol         -> UnicodeSymbol
-      Ch.ModifierSymbol         -> UnicodeSymbol
-      Ch.OtherSymbol            -> UnicodeSymbol
+generalCategoryMinorLetter Ch.MathSymbol             = 'm'
+generalCategoryMinorLetter Ch.CurrencySymbol         = 'c'
+generalCategoryMinorLetter Ch.ModifierSymbol         = 'k'
+generalCategoryMinorLetter Ch.OtherSymbol            = 'o'
 
-      Ch.Space                  -> UnicodeSeperator
-      Ch.LineSeparator          -> UnicodeSeperator
-      Ch.ParagraphSeparator     -> UnicodeSeperator
+generalCategoryMinorLetter Ch.Space                  = 's'
+generalCategoryMinorLetter Ch.LineSeparator          = 'l'
+generalCategoryMinorLetter Ch.ParagraphSeparator     = 'p'
 
-      Ch.Control                -> UnicodeOther
-      Ch.Format                 -> UnicodeOther
-      Ch.Surrogate              -> UnicodeOther
-      Ch.PrivateUse             -> UnicodeOther
-      Ch.NotAssigned            -> UnicodeOther
+generalCategoryMinorLetter Ch.Control                = 'c'
+generalCategoryMinorLetter Ch.Format                 = 'f'
+generalCategoryMinorLetter Ch.Surrogate              = 's'
+generalCategoryMinorLetter Ch.PrivateUse             = 'o'
+generalCategoryMinorLetter Ch.NotAssigned            = 'n'
 
