87e7f46f047c0b518ed9ae667eb2e77676aaffb0
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-04-27 18:11:18 +0900

    group relation modules

---
+++ baala/data/Koshucode/Baala/Data/Type/Rel.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Koshucode.Baala.Data.Type.Rel
+  ( module Koshucode.Baala.Data.Type.Rel.Head,
+    module Koshucode.Baala.Data.Type.Rel.Mono,
+    module Koshucode.Baala.Data.Type.Rel.Rel,
+  ) where
+
+import Koshucode.Baala.Data.Type.Rel.Head
+import Koshucode.Baala.Data.Type.Rel.Mono
+import Koshucode.Baala.Data.Type.Rel.Rel
+
+
