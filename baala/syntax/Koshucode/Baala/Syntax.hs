0406d57c7131702b99b00547c3468e7c0110aab7
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-04-27 13:33:53 +0900

    add syntax bundle module

---
+++ baala/syntax/Koshucode/Baala/Syntax.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Koshucode.Baala.Syntax
+  ( module Koshucode.Baala.Syntax.Attr,
+    module Koshucode.Baala.Syntax.Symbol,
+    module Koshucode.Baala.Syntax.Token,
+  ) where
+
+import Koshucode.Baala.Syntax.Attr
+import Koshucode.Baala.Syntax.Symbol
+import Koshucode.Baala.Syntax.Token
+
