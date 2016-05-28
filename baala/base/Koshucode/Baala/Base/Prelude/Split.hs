a53aa85ddf1ea0e00069afb1c2515fd1ff98bab7
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-05-28 15:01:30 +0900

    add type for split function

--- baala/base/Koshucode/Baala/Base/Prelude/Split.hs
+++ baala/base/Koshucode/Baala/Base/Prelude/Split.hs
@@ -3,16 +3,45 @@
 -- | Split lists.
 
 module Koshucode.Baala.Base.Prelude.Split
-  ( Split,
+  ( -- * Type
+    Split,
+    Split2, SplitList2,
+    Split3', SplitList3',
+    Split3, SplitList3,
+
+    -- * Function
     chunks,
     splitBy,
     divide, divideBy,
     wordsBy,
   ) where
 
+-- --------------------------------------------  Type
+
 -- | Split list into before-part, split-element and after-part (@Right@),
 --   or original list if not splittable (@Left@).
 type Split a = [a] -> Either [a] ([a], a, [a])
+
+-- | Split elements into two subelements.
+type Split2 t a = t a -> Maybe (t a, t a)
+
+-- | Split list into two sublists.
+type SplitList2 a = Split2 [] a
+
+-- | Split elements into before-elem-after parts.
+type Split3' t a = t a -> Maybe (t a, a, t a)
+
+-- | Split list into before-elem-after parts.
+type SplitList3' a = Split3' [] a
+
+-- | Split elements into three subelements.
+type Split3 t a = t a -> Maybe (t a, t a, t a)
+
+-- | Split list into three sublists.
+type SplitList3 a = Split3 [] a
+
+
+-- --------------------------------------------  Function
 
 chunks :: Int -> [a] -> [[a]]
 chunks n = loop where
