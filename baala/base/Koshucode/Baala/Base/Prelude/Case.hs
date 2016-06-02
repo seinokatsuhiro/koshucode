61d72969b97c30c2e0e4e5c606e5cc886a4ba0ec
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-06-02 10:23:51 +0900

    add case-zero functions

--- baala/base/Koshucode/Baala/Base/Prelude/Case.hs
+++ baala/base/Koshucode/Baala/Base/Prelude/Case.hs
@@ -6,6 +6,13 @@ module Koshucode.Baala.Base.Prelude.Case
   ( -- * Null
     caseNull,
     headNull, tailNull,
+    minimumNull, maximumNull,
+
+    -- * Zero
+    caseZero,
+    quotZero, remZero,
+    divZero, modZero,
+    quotRemZero, divModZero,
   ) where
 
 
@@ -45,4 +52,53 @@ minimumNull = caseNull minimum
 -- | Case-null version of 'maximum'.
 maximumNull :: (Ord a) => a -> [a] -> a
 maximumNull = caseNull maximum
+
+
+-- --------------------------------------------  Zero
+
+-- | Apply function unless 0.
+caseZero :: (Eq a, Num a) => (a -> b) -> b -> a -> b
+caseZero f y x | x == 0     = y
+               | otherwise  = f x
+
+caseZeroBy :: (Eq a, Num a) => (a -> a -> b) -> b -> a -> a -> b
+caseZeroBy f y n = caseZero (f n) y
+
+-- | Case-zero version of 'quot'.
+quotZero :: (Integral a) => a -> a -> a -> a
+quotZero = caseZeroBy quot
+
+-- | Case-zero version of 'rem'.
+remZero :: (Integral a) => a -> a -> a -> a
+remZero = caseZeroBy rem
+
+-- | Case-zero version of 'div'.
+--
+--   >>> divZero 0 10 2 :: Int
+--   5
+--
+--   >>> divZero 0 10 0 :: Int
+--   0
+
+divZero :: (Integral a) => a -> a -> a -> a
+divZero = caseZeroBy div
+
+-- | Case-zero version of 'mod'.
+modZero :: (Integral a) => a -> a -> a -> a
+modZero = caseZeroBy mod
+
+-- | Case-zero version of 'quotRem'.
+--
+--   >>> quotRemZero (0,0) 20 6 :: (Int, Int)
+--   (3,2)
+--
+--   >>> quotRemZero (0,0) 20 0 :: (Int, Int)
+--   (0,0)
+--
+quotRemZero :: (Integral a) => (a, a) -> a -> a -> (a, a)
+quotRemZero = caseZeroBy quotRem
+
+-- | Case-zero version of 'divMod'.
+divModZero :: (Integral a) => (a, a) -> a -> a -> (a, a)
+divModZero = caseZeroBy divMod
 
