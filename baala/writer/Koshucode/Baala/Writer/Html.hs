73bc833532b8b235b1fb3bd07851c3a5fb5bab8e
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-06-15 14:27:57 +0900

    reimplement html output

--- baala/writer/Koshucode/Baala/Writer/Html.hs
+++ baala/writer/Koshucode/Baala/Writer/Html.hs
@@ -10,26 +10,43 @@ import qualified Text.Blaze.XHtml5                  as H
 import           Text.Blaze.XHtml5                  ((!))
 import           Text.Blaze.XHtml5.Attributes       (class_)
 import qualified Koshucode.Baala.Base               as B
+import qualified Koshucode.Baala.Data               as D
 import qualified Koshucode.Baala.Syntax             as S
 import qualified Koshucode.Baala.Core               as C
 
-resultHtmlIndented :: (B.Write c) => C.ResultWriter c
+resultHtmlIndented :: (D.CContent c) => C.ResultWriter c
 resultHtmlIndented = C.ResultWriterChunk "html-indented" (hPutHtml B.renderHtmlIndented)
 
-resultHtmlCompact :: (B.Write c) => C.ResultWriter c
+resultHtmlCompact :: (D.CContent c) => C.ResultWriter c
 resultHtmlCompact  = C.ResultWriterChunk "html-compact"  (hPutHtml B.renderHtmlCompact)
 
-hPutHtml :: (B.Write c) => (H.Html -> String) -> C.ResultWriterChunk c
+hPutHtml :: (D.CContent c) => (H.Html -> String) -> C.ResultWriterChunk c
 hPutHtml render h _ status sh =
     do hPutRel h render sh
        return status
 
-hPutRel :: (B.Write c) => IO.Handle -> (H.Html -> String) -> [C.ShortResultChunks c] -> IO ()
+hPutRel :: (D.CContent c) => IO.Handle -> (H.Html -> String) -> [C.ShortResultChunks c] -> IO ()
 hPutRel h render sh = mapM_ put chunks where
     chunks = concatMap S.shortBody sh
-    put (C.ResultRel pat r) = IO.hPutStrLn h $ render $ html pat r
-    put _                 = return ()
-    html pat r = H.div ! class_ "named-relation" $ do
-                   H.p ! class_ "name" $ H.toHtml pat
-                   B.writeHtmlWith B.noShorten r
+    put (C.ResultRel cl r) = IO.hPutStrLn h $ render $ html cl r
+    put _                  = return ()
+    html cl r = H.div ! class_ "named-relation" $ do
+                  H.p ! class_ "name" $ H.toHtml cl
+                  contToHtml B.noShorten $ D.pRel r
+
+contToHtml :: (D.CContent c) => B.Shorten -> c -> H.Html
+contToHtml sh = content where
+    content c
+        | D.isRel c = rel $ D.gRel c
+        | otherwise = B.toHtml $ B.mixToFlatString $ B.mixShortEncode sh c
+
+    rel (D.Rel he bo) =
+        H.table ! class_ "relation" $ do
+          let terms = term `map` D.headNames he
+          H.tr ! class_ "heading" $ mapM_ H.td terms
+          mapM_ row bo
+
+    row cs = H.tr ! class_ "tuple" $ mapM_ col cs
+    col c  = H.td $ contToHtml sh c
+    term   = (H.span ! class_ "termname") . H.toHtml . S.showTermName
 
