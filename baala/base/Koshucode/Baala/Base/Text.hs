10588277ac323a3d2650c3565f891fa5f7c980ad
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-06-19 09:27:34 +0900

    remove html module from base package

--- baala/base/Koshucode/Baala/Base/Text.hs
+++ baala/base/Koshucode/Baala/Base/Text.hs
@@ -2,7 +2,6 @@
 
 module Koshucode.Baala.Base.Text
   ( module Koshucode.Baala.Base.Text.Comment,
-    module Koshucode.Baala.Base.Text.Html,
     module Koshucode.Baala.Base.Text.Http,
     module Koshucode.Baala.Base.Text.TextTable,
     module Koshucode.Baala.Base.Text.ToDoc,
@@ -11,7 +10,6 @@ module Koshucode.Baala.Base.Text
   ) where
 
 import Koshucode.Baala.Base.Text.Comment
-import Koshucode.Baala.Base.Text.Html
 import Koshucode.Baala.Base.Text.Http
 import Koshucode.Baala.Base.Text.TextTable
 import Koshucode.Baala.Base.Text.ToDoc
