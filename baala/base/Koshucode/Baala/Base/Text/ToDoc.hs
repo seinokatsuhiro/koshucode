052ffa3b140bf72b22f47abaa725b28c1ba79ca9
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-07-03 11:39:17 +0900

    add comments

--- baala/base/Koshucode/Baala/Base/Text/ToDoc.hs
+++ baala/base/Koshucode/Baala/Base/Text/ToDoc.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wall #-}
 
--- | Convert to 'B.Doc'.
+-- | Convert to 'B.Doc' pretty printer.
 
 module Koshucode.Baala.Base.Text.ToDoc
   ( ToDoc (..),
@@ -13,31 +13,47 @@ import qualified Text.PrettyPrint                 as Pr
 import qualified Koshucode.Baala.Base.Prelude     as B
 import qualified Koshucode.Baala.Base.List        as B
 
--- | Convert to pretty print 'B.Doc'.
+-- | Convert to 'B.Doc' pretty printer.
 class ToDoc a where
     doc :: a -> B.Doc
 
--- | Identity.
+-- | No conversion.
 instance ToDoc B.Doc where
     doc = id
 
--- | Same as 'Pr.int'.
+-- | 'Int' printer, same as 'Pr.int'.
 instance ToDoc Int where
     doc = Pr.int
 
--- | Same as 'Pr.intteger'.
+-- | 'Integer' printer, same as 'Pr.integer'.
 instance ToDoc Integer where
     doc = Pr.integer
 
--- | Same as 'Pr.text'.
+-- | 'String' printer, same as 'Pr.text'.
 instance ToDoc String where
     doc = Pr.text
+
+-- | Concatenate docs horizontally with space, same as 'Pr.hsep'.
+--
+--   >>> doch ["abc", "def"]
+--   abc def
+
+doch :: (ToDoc a) => [a] -> B.Doc
+doch = Pr.hsep . map doc
+
+-- | Concatenate docs vertically, same as 'Pr.vcat'.
+--
+--   >>> docv ["abc", "def"]
+--   abc
+--   def
 
 docv :: (ToDoc a) => [a] -> B.Doc
 docv = Pr.vcat . map doc
 
-doch :: (ToDoc a) => [a] -> B.Doc
-doch = Pr.hsep . map doc
+-- | Wrap doc with open and close docs.
+--
+--   >>> docWraps "(" ")" "abc"
+--   ( abc )
 
 docWraps :: (ToDoc a, ToDoc b) => a -> a -> b -> B.Doc
 docWraps = docWrapBody (B.<+>)
