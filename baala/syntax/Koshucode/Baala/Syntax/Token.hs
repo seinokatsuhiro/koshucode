592b440de52cb0aba4e1221ed2ae35a143ee7b4e
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-07-04 05:21:25 +0900

    remove token line module

--- baala/syntax/Koshucode/Baala/Syntax/Token.hs
+++ baala/syntax/Koshucode/Baala/Syntax/Token.hs
@@ -6,13 +6,11 @@ module Koshucode.Baala.Syntax.Token
   ( module Koshucode.Baala.Syntax.Token.Pattern,
     module Koshucode.Baala.Syntax.Token.Token,
     module Koshucode.Baala.Syntax.Token.TokenClause,
-    module Koshucode.Baala.Syntax.Token.TokenLine,
     module Koshucode.Baala.Syntax.Token.Utility,
   ) where
 
 import Koshucode.Baala.Syntax.Token.Pattern
 import Koshucode.Baala.Syntax.Token.Token
 import Koshucode.Baala.Syntax.Token.TokenClause
-import Koshucode.Baala.Syntax.Token.TokenLine
 import Koshucode.Baala.Syntax.Token.Utility
 
