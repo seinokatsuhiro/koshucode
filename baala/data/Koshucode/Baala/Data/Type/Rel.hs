aec57d84e6f4498ae2c35062f101c7ab089fef9d
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-07-09 01:38:19 +0900

    create share side module

--- baala/data/Koshucode/Baala/Data/Type/Rel.hs
+++ baala/data/Koshucode/Baala/Data/Type/Rel.hs
@@ -1,14 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
 
 -- | Relation type.
+
 module Koshucode.Baala.Data.Type.Rel
   ( module Koshucode.Baala.Data.Type.Rel.Head,
     module Koshucode.Baala.Data.Type.Rel.Mono,
     module Koshucode.Baala.Data.Type.Rel.Rel,
+    module Koshucode.Baala.Data.Type.Rel.ShareSide,
   ) where
 
 import Koshucode.Baala.Data.Type.Rel.Head
 import Koshucode.Baala.Data.Type.Rel.Mono
 import Koshucode.Baala.Data.Type.Rel.Rel
-
+import Koshucode.Baala.Data.Type.Rel.ShareSide
 
--- baala/data/Koshucode/Baala/Data/Type/Rel/Head.hs
+++ baala/data/Koshucode/Baala/Data/Type/Rel/Head.hs
@@ -32,12 +32,6 @@ module Koshucode.Baala.Data.Type.Rel.Head
     headUp,
     bodyAlign,
     -- $Mapping
-
-    -- * Picker
-    HeadLR (..),
-    HeadLRMap,
-    HeadLRMap2,
-    headLR, headLROrd,
   ) where
 
 import qualified Koshucode.Baala.Base              as B
@@ -213,77 +207,3 @@ headAlign to from = B.snipOrder (D.getTermNames to) (D.getTermNames from)
 bodyAlign :: Head -> Head -> B.Map [[c]]
 bodyAlign to from = (headAlign to from `map`)
 
-
--- ----------------------  Picker
-
-type HeadLRMap c = HeadLR c -> [c] -> [c]
-type HeadLRMap2 a b = (HeadLRMap a, HeadLRMap b)
-
-data HeadLR c = HeadLR
-    { headLShareIndex  :: [Int]         -- ^ Indicies of right-shared part
-    , headRShareIndex  :: [Int]         -- ^ Indicies of left-shared part
-    , headDisjoint     :: Bool          -- ^ Whether shared part is empty
-
-    , headLSideNames   :: [S.TermName]  -- ^ Left-side term names
-    , headLShareNames  :: [S.TermName]  -- ^ Left-shared term names
-    , headRShareNames  :: [S.TermName]  -- ^ Right-shared term names
-    , headRSideNames   :: [S.TermName]  -- ^ Right-side term names
-
-    , headLSide        :: [c] -> [c]    -- ^ Pick left-side part from left contents
-    , headLShare       :: [c] -> [c]    -- ^ Pick left-shared part from left contents
-    , headRShare       :: [c] -> [c]    -- ^ Pick right-shared part from right contents
-    , headRSide        :: [c] -> [c]    -- ^ Pick right-side part from right contents
-
-    , headRForward     :: [c] -> [c]
-    , headRBackward    :: [c] -> [c]
-
-    , headRSplit       :: [c] -> ([c], [c])  -- ^ Pick right-shared and right-side part
-    , headRAssoc       :: [c] -> ([c], [c])  -- ^ Pick right-shared part and right contents
-    }
-
-headLR :: [S.TermName] -> [S.TermName] -> HeadLR a
-headLR left right = headLRBody li ri left right where
-    (li, ri) = sharedIndex left right
-
--- sharedIndex "dxcy" "abcd"
--- >>> ([0,2], [3,2])
---       d c    d c
-sharedIndex :: (Ord a) => [a] -> [a] -> ([Int], [Int])
-sharedIndex xs1 xs2 = (ind1, ind2) where
-    ind1  = B.snipIndex sh xs1
-    ind2  = B.snipIndex sh xs2
-    sh    = B.intersectionFilter xs2 xs1
-
-headLROrd :: [S.TermName] -> [S.TermName] -> HeadLR a
-headLROrd left right = headLRBody li ri left right where
-    (li, ri)  = (ind2 left, ind2 right)
-    ind       = B.snipIndex left right
-    ind2      = B.snipIndex $ B.snipFrom ind right
-
-headLRBody :: [Int] -> [Int] -> [S.TermName] -> [S.TermName] -> HeadLR a
-headLRBody li ri left right = lr where
-    lside      = B.snipOff  li
-    lshare     = B.snipFrom li
-    rshare     = B.snipFrom ri
-    rside      = B.snipOff  ri
-    rfor       = B.snipForward  ri
-    rback      = B.snipBackward ri
-    rsplit xs  = (rshare xs, rside xs)
-    rassoc xs  = (rshare xs, xs)
-
-    lr = HeadLR { headLShareIndex  = li
-                , headRShareIndex  = ri
-                , headDisjoint     = null li
-                , headLSideNames   = lside  left
-                , headLShareNames  = lshare left
-                , headRShareNames  = rshare right
-                , headRSideNames   = rside  right
-                , headLSide        = lside
-                , headLShare       = lshare
-                , headRShare       = rshare
-                , headRSide        = rside
-                , headRForward     = rfor
-                , headRBackward    = rback
-                , headRSplit       = rsplit
-                , headRAssoc       = rassoc
-                }
