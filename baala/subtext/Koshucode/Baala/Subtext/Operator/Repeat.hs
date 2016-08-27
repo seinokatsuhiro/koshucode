867dc73b42e37916fd3e2bb623ee9983bd563f09
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-08-27 14:33:53 +0900

    repetitive operators

---
+++ baala/subtext/Koshucode/Baala/Subtext/Operator/Repeat.hs
@@ -0,0 +1,45 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Repetitive operators.
+
+module Koshucode.Baala.Subtext.Operator.Repeat
+ ( -- * Repetitive
+   min, max, minMax,
+   many, many1, maybe,
+   before,
+ ) where
+
+import Prelude hiding ( min, max, maybe )
+
+import qualified Koshucode.Baala.Subtext.Expr                as S
+import qualified Koshucode.Baala.Subtext.Operator.Basic      as S
+import qualified Koshucode.Baala.Subtext.Operator.Combine    as S
+
+-- | Repetition with lower bound.
+min :: Int -> S.Expr a -> S.Expr a
+min a = S.ERec . S.ERep (S.Min a)
+
+-- | Repetition upper bound.
+max :: Int -> S.Expr a -> S.Expr a
+max = minMax 0
+
+-- | Repetition with lower and upper bound.
+minMax :: Int -> Int -> S.Expr a -> S.Expr a
+minMax a b = S.ERec . S.ERep (S.MinMax a b)
+
+-- | Match zero or more times.
+many :: S.Expr a -> S.Expr a
+many = min 0
+
+-- | Match one or more times.
+many1 :: S.Expr a -> S.Expr a
+many1 = min 1
+
+-- | Match zero or one times.
+maybe :: S.Expr a -> S.Expr a
+maybe = minMax 0 1
+
+-- | Match before given expression.
+before :: S.Expr a -> S.Expr a
+before e = many $ S.not S.any [e]
+
