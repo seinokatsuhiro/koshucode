a8aeff1fa2dd44bf73e00d7577c6c6c1db087357
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-08-27 20:59:22 +0900

    create subtext module

---
+++ baala/subtext/Koshucode/Baala/Subtext.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | A text matching library.
+
+module Koshucode.Baala.Subtext
+  ( module Koshucode.Baala.Subtext.Match,
+    module Koshucode.Baala.Subtext.Operator,
+  ) where
+
+import Koshucode.Baala.Subtext.Match
+import Koshucode.Baala.Subtext.Operator
+
