19321f07ea50214bd43596cad670d439523402b1
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-08-27 14:54:00 +0900

    match operators

---
+++ baala/subtext/Koshucode/Baala/Subtext/Operator.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Match operators.
+
+module Koshucode.Baala.Subtext.Operator
+ ( module Koshucode.Baala.Subtext.Operator.Basic,
+   module Koshucode.Baala.Subtext.Operator.Char,
+   module Koshucode.Baala.Subtext.Operator.Combine,
+   module Koshucode.Baala.Subtext.Operator.Repeat,
+ ) where
+
+import Koshucode.Baala.Subtext.Operator.Basic
+import Koshucode.Baala.Subtext.Operator.Char
+import Koshucode.Baala.Subtext.Operator.Combine
+import Koshucode.Baala.Subtext.Operator.Repeat
+
