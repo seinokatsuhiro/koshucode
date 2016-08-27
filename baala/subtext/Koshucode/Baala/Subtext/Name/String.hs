ca54ebcf16fdb5252042d81eeee84939a40c751a
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-08-27 15:28:36 +0900

    create name modules

---
+++ baala/subtext/Koshucode/Baala/Subtext/Name/String.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | String name.
+
+module Koshucode.Baala.Subtext.Name.String
+ ( Name,
+   GetName (..),
+ ) where
+
+-- | String name.
+type Name = String
+
+-- | Class for types which has name.
+class GetName a where
+    getName :: a -> Name
+
---
+++ baala/subtext/Koshucode/Baala/Subtext/Name/Text.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Text name.
+
+module Koshucode.Baala.Subtext.Name.Text
+ ( Name,
+   GetName (..),
+ ) where
+
+import Data.Text
+
+-- | Text name.
+type Name = Text
+
+-- | Class for types which has name.
+class GetName a where
+    getName :: a -> Name
+
