c225a1b2a8534cb5e7e8ff7d1ee67202e609f34d
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-08-28 14:31:27 +0900

    create int-string module

---
+++ baala/subtext/Koshucode/Baala/Subtext/Name/IntString.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Integer or string name.
+
+module Koshucode.Baala.Subtext.Name.IntString
+ ( Name (..),
+   GetName (..),
+ ) where
+
+-- | String or integer name.
+data Name
+    = NameI Int
+    | NameS String 
+      deriving (Show, Eq, Ord)
+
+-- | Class for types which has name.
+class GetName a where
+    getName :: a -> Name
+
