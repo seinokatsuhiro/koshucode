0122a0a9ec564903cf7120fad7591a705bd58035
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-09-18 12:40:54 +0900

    add list version of not

--- baala/subtext/Koshucode/Baala/Subtext/Operator/Combine.hs
+++ baala/subtext/Koshucode/Baala/Subtext/Operator/Combine.hs
@@ -4,7 +4,7 @@
 
 module Koshucode.Baala.Subtext.Operator.Combine
  ( -- * Combination
-   or, seq, not,
+   or, seq, not, not',
 
    -- * Variation
    peek, skip, gather,
@@ -13,23 +13,40 @@ module Koshucode.Baala.Subtext.Operator.Combine
 
 import Prelude hiding ( or, seq, not )
 
-import qualified Koshucode.Baala.Subtext.Fn    as S
-import qualified Koshucode.Baala.Subtext.Expr  as S
+import qualified Koshucode.Baala.Subtext.Fn              as S
+import qualified Koshucode.Baala.Subtext.Expr            as S
+import qualified Koshucode.Baala.Subtext.Operator.Basic  as S
 
 
 -- --------------------------------------------  Combination
 
 -- | Alternative match.
 or :: [S.Expr a] -> S.Expr a
-or = S.ERec . S.EOr
+or []  = S.fail
+or [e] = e
+or es  = S.ERec $ S.EOr es
 
 -- | Sequential match.
 seq :: [S.Expr a] -> S.Expr a
-seq = S.ERec . S.ESeq
+seq []  = S.succ
+seq [e] = e
+seq es  = S.ERec $ S.ESeq es
 
 -- | Match with exceptions.
 not :: S.Expr a -> [S.Expr a] -> S.Expr a
+not e [] = e
 not e es = S.ERec $ S.ENot e es
+
+-- | List version of 'not'.
+--
+--   * @not' [/E/, /E1/, /E2/]@ == @not /E/ [/E1/, /E2/]@
+--   * @not' [/E/]@ == @/E/@
+--   * @not' []@ == @seq []@ == @succ@
+
+not' :: [S.Expr a] -> S.Expr a
+not' []        = S.succ
+not' [e]       = e
+not' (e : es)  = not e es
 
 
 -- --------------------------------------------  Variation
