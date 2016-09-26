de5c5db20fda9af4b38cf402ec8581cbaa1b562f
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-09-26 22:25:04 +0900

    function for unicode general category

--- baala/subtext/Koshucode/Baala/Subtext/Operator/Char.hs
+++ baala/subtext/Koshucode/Baala/Subtext/Operator/Char.hs
@@ -7,9 +7,11 @@ module Koshucode.Baala.Subtext.Operator.Char
    char, word,
    space, spaces, spaces1,
    digit, letter,
+   categorySet, category, categoryLookup,
  ) where
 
 import qualified Data.Char                                   as C
+import qualified Data.Set                                    as Set
 import qualified Koshucode.Baala.Subtext.Expr                as S
 import qualified Koshucode.Baala.Subtext.Operator.Basic      as S
 import qualified Koshucode.Baala.Subtext.Operator.Repeat     as S
@@ -43,3 +45,87 @@ digit = S.elem "digit" C.isDigit
 letter :: S.Expr Char
 letter = S.elem "letter" C.isLetter
 
+-- | Test unicode general category with category set.
+categorySet :: Set.Set C.GeneralCategory -> S.Expr Char
+categorySet cs = S.elem "category" cat where
+    cat c = C.generalCategory c `Set.member` cs
+
+-- | Test unicode general category with short names.
+--   The argument string is space-separated names.
+--   If unknown name is given, returns the name in 'Left' data.
+category :: String -> Either String (S.Expr Char)
+category s =
+    do cs <- categoryLookup `mapM` words s
+       Right $ categorySet $ Set.unions cs
+
+-- | Lookup unicode general cateogries by its short name.
+--   Short name means two-letter category name
+--   (e.g., @"lu"@ for upper case letter, @"zs"@ for space), or
+--   one-letter category group (e.g., @"l"@ for letter, @"z"@ for separator).
+--
+--   >>> categoryLookup "lu"
+--   Right (fromList [UppercaseLetter])
+--
+--   >>> categoryLookup "n"
+--   Right (fromList [DecimalNumber, LetterNumber, OtherNumber])
+--
+--   >>> categoryLookup "lx"
+--   Left "lx"
+
+categoryLookup :: String -> Either String (Set.Set C.GeneralCategory)
+categoryLookup n =
+    case C.toLower <$> n of
+      "l"     -> l [ C.UppercaseLetter, C.LowercaseLetter, C.TitlecaseLetter,
+                     C.ModifierLetter, C.OtherLetter ]
+      "m"     -> l [ C.NonSpacingMark, C.SpacingCombiningMark, C.EnclosingMark ]
+      "n"     -> l [ C.DecimalNumber, C.LetterNumber, C.OtherNumber ]
+      "p"     -> l [ C.ConnectorPunctuation, C.DashPunctuation,
+                     C.OpenPunctuation, C.ClosePunctuation, C.InitialQuote,
+                     C.FinalQuote, C.OtherPunctuation ]
+      "s"     -> l [ C.MathSymbol, C.CurrencySymbol,
+                     C.ModifierSymbol, C.OtherSymbol ]
+      "z"     -> l [ C.Space, C.LineSeparator, C.ParagraphSeparator ]
+      "c"     -> l [ C.Control, C.Format, C.Surrogate,
+                     C.PrivateUse, C.NotAssigned ]
+
+      "lu"    -> s C.UppercaseLetter
+      "ll"    -> s C.LowercaseLetter
+      "lt"    -> s C.TitlecaseLetter
+      "lm"    -> s C.ModifierLetter
+      "lo"    -> s C.OtherLetter
+
+      "mn"    -> s C.NonSpacingMark
+      "mc"    -> s C.SpacingCombiningMark
+      "me"    -> s C.EnclosingMark
+
+      "nd"    -> s C.DecimalNumber
+      "nl"    -> s C.LetterNumber
+      "no"    -> s C.OtherNumber
+
+      "pc"    -> s C.ConnectorPunctuation
+      "pd"    -> s C.DashPunctuation
+      "ps"    -> s C.OpenPunctuation
+      "pe"    -> s C.ClosePunctuation
+      "pi"    -> s C.InitialQuote
+      "pf"    -> s C.FinalQuote
+      "po"    -> s C.OtherPunctuation
+
+      "sm"    -> s C.MathSymbol
+      "sc"    -> s C.CurrencySymbol
+      "sk"    -> s C.ModifierSymbol
+      "so"    -> s C.OtherSymbol
+
+      "zs"    -> s C.Space
+      "zl"    -> s C.LineSeparator
+      "zp"    -> s C.ParagraphSeparator
+
+      "cc"    -> s C.Control
+      "cf"    -> s C.Format
+      "cs"    -> s C.Surrogate
+      "co"    -> s C.PrivateUse
+      "cn"    -> s C.NotAssigned
+
+      _       -> Left n
+    where
+      l = Right . Set.fromList
+      s = Right . Set.singleton
