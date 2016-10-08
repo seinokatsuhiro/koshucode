7a64c279e1a82d7c8431feb26e0d876055e0e989
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-10-08 13:42:44 +0900

    repeat with min-max parameter

--- baala/subtext/Koshucode/Baala/Subtext/Operator/Repeat.hs
+++ baala/subtext/Koshucode/Baala/Subtext/Operator/Repeat.hs
@@ -3,22 +3,42 @@
 -- | Repetitive operators.
 
 module Koshucode.Baala.Subtext.Operator.Repeat
- ( -- * Repetitive
+ ( -- * Basic repetition
+   repeat, repeatTimes, repeatExpr,
    min, max, minMax,
+
+   -- * Derived repetition
    many, many1, maybe,
    anySeq, before, sep,
  ) where
 
-import Prelude hiding ( min, max, maybe )
+import Prelude hiding ( repeat, min, max, maybe )
 
 import qualified Koshucode.Baala.Subtext.Expr                as S
 import qualified Koshucode.Baala.Subtext.MinMax              as S
 import qualified Koshucode.Baala.Subtext.Operator.Basic      as S
 import qualified Koshucode.Baala.Subtext.Operator.Combine    as S
 
+
+-- ----------------------  Basic repetition
+
+-- | Repetition with min-max parameter.
+repeat :: S.MinMax -> S.Expr a -> S.Expr a
+repeat m = S.ERec . S.ERep m
+
+-- | Extract min-max parameter.
+repeatTimes :: S.Expr a -> Maybe S.MinMax
+repeatTimes (S.ERec (S.ERep t _))  = Just t
+repeatTimes _                      = Nothing
+
+-- | Extract internal expression.
+repeatExpr :: S.Expr a -> Maybe (S.Expr a)
+repeatExpr (S.ERec (S.ERep _ e))   = Just e
+repeatExpr _                       = Nothing
+
 -- | Repetition with lower bound.
 min :: Int -> S.Expr a -> S.Expr a
-min a = S.ERec . S.ERep (S.Min a)
+min a = repeat $ S.Min a
 
 -- | Repetition upper bound.
 max :: Int -> S.Expr a -> S.Expr a
@@ -26,7 +46,10 @@ max = minMax 0
 
 -- | Repetition with lower and upper bound.
 minMax :: Int -> Int -> S.Expr a -> S.Expr a
-minMax a b = S.ERec . S.ERep (S.MinMax a b)
+minMax a b = repeat $ S.MinMax a b
+
+
+-- ----------------------  Derived repetition
 
 -- | Match zero or more times.
 many :: S.Expr a -> S.Expr a
