ee99c25cfa78d15c78081ec904ad902aa58c6188
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-10-16 21:11:03 +0900

    create text module

--- baala/overture/Koshucode/Baala/Overture.hs
+++ baala/overture/Koshucode/Baala/Overture.hs
@@ -3,13 +3,9 @@
 -- | Bundle of overture modules.
 
 module Koshucode.Baala.Overture
- ( module Koshucode.Baala.Overture.Text.Category,
-   module Koshucode.Baala.Overture.Text.Unicode,
-   module Koshucode.Baala.Overture.Text.Utility,
+ ( module Koshucode.Baala.Overture.Text,
    module Koshucode.Baala.Overture.Type,
  ) where
 
-import Koshucode.Baala.Overture.Text.Category
-import Koshucode.Baala.Overture.Text.Unicode
-import Koshucode.Baala.Overture.Text.Utility
+import Koshucode.Baala.Overture.Text
 import Koshucode.Baala.Overture.Type
---
+++ baala/overture/Koshucode/Baala/Overture/Text.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Bundle of text modules.
+
+module Koshucode.Baala.Overture.Text
+ ( module Koshucode.Baala.Overture.Text.Category,
+   module Koshucode.Baala.Overture.Text.Unicode,
+   module Koshucode.Baala.Overture.Text.Utility,
+ ) where
+
+import Koshucode.Baala.Overture.Text.Category
+import Koshucode.Baala.Overture.Text.Unicode
+import Koshucode.Baala.Overture.Text.Utility
