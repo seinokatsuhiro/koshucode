f5e72494292e8e325f07f9d6bb32576d09b2eecc
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-04 05:25:16 +0900

    create module for derived mix text

--- baala/base/Koshucode/Baala/Base/MixText.hs
+++ baala/base/Koshucode/Baala/Base/MixText.hs
@@ -4,12 +4,14 @@
 
 module Koshucode.Baala.Base.MixText
   ( module Koshucode.Baala.Base.MixText.LineBreak,
+    module Koshucode.Baala.Base.MixText.Deriv,
     module Koshucode.Baala.Base.MixText.MixClass,
     module Koshucode.Baala.Base.MixText.MixText,
     module Koshucode.Baala.Base.MixText.MixEncode,
   ) where
 
 import Koshucode.Baala.Base.MixText.LineBreak
+import Koshucode.Baala.Base.MixText.Deriv
 import Koshucode.Baala.Base.MixText.MixClass
 import Koshucode.Baala.Base.MixText.MixText
 import Koshucode.Baala.Base.MixText.MixEncode
---
+++ baala/base/Koshucode/Baala/Base/MixText/Deriv.hs
@@ -0,0 +1,55 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Derived mix text.
+
+module Koshucode.Baala.Base.MixText.Deriv
+  ( mixTab,
+    mixTerms,
+    mixJudge,
+  ) where
+
+import Data.Monoid ((<>))
+import qualified Koshucode.Baala.Base.MixText.MixText         as B
+import qualified Koshucode.Baala.Base.MixText.MixClass        as B
+
+-- | Mix text tab character.
+--
+--   >>> B.mixString "a" `mixTab` B.mixString "b"
+--   MixText "a\tb"
+--
+mixTab :: B.MixText
+mixTab = B.mixChar '\t'
+
+-- | Join mix text list by separetor.
+--
+--   >>> mixJoin B.mix1 ["foo", "bar", "baz"]
+--   MixText "foo bar baz"
+--
+--   >>> mixJoin mixTab ["foo", "bar", "baz"]
+--   MixText "foo\tbar\tbaz\t"
+--
+mixJoin :: (B.Mix a, B.Mix b) => a -> [b] -> B.MixText
+mixJoin sep = loop where
+    loop [] = B.mixEmpty
+    loop (m:ms) = B.mix m <> B.mix sep <> loop ms
+
+-- | Concatenate terms of content.
+--
+--  >>> mixTerms B.mix2 [("a", "foo"), ("b", "bar")]
+--  MixText "/a foo  /b bar"
+--
+mixTerms :: (B.Mix sep,  B.Mix c) => sep -> [(String, c)] -> B.MixText
+mixTerms sep ts = mixJoin sep (f <$> ts) where
+    f (n, c) = mixTermName n `B.mixSep` B.mix c
+
+mixTermName :: String -> B.MixText
+mixTermName n = B.mixString ('/' : n)
+
+-- | Create judgement using mix text.
+--
+--  >>> mixJudge B.mix2 "AB" [("a", "foo"), ("b", "bar")]
+--  MixText "|-- AB  /a foo  /b bar"
+--
+mixJudge :: (B.Mix sep, B.Mix cl, B.Mix c) => sep -> cl -> [(String, c)] -> B.MixText
+mixJudge sep cl ts = B.mixString "|-- " <> B.mix cl <> B.mix sep <> mixTerms sep ts
+
