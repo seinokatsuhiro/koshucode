7211cb2ddb039aa6f50b274cca51b1be666b6aa7
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-10 07:51:58 +0900

    add examples of simple option module

--- baala/base/Koshucode/Baala/Base/IO/SimpleOption.hs
+++ baala/base/Koshucode/Baala/Base/IO/SimpleOption.hs
@@ -7,22 +7,23 @@ module Koshucode.Baala.Base.IO.SimpleOption
  ( -- * Data type
    ShortOption, LongOption,
    SimpleOption,
-   SimpleOptions,
-   SimpleOptionDescr,
    SimpleOption',
+   SimpleOptionDescr,
+   SimpleOptions,
 
    -- * Option type
    flag, opt, req,
    getFlag, getOpt, getReq,
 
-   -- * Predefined option
-   help, version,
-   printHelp, helpMessage,
-   Ver.showVersion,
-
    -- * Parsing
    ParseResult', ParseResult,
    parse, parseCommand,
+
+   -- * Predefined option
+   -- ** --help
+   help, helpMessage, printHelp,
+   -- ** --version
+   version, Ver.showVersion,
  ) where
 
 import qualified Data.Maybe            as Maybe
@@ -42,44 +43,55 @@ type LongOption = String
 -- | Simple option.
 type SimpleOption = SimpleOption' LongOption
 
--- | Option list of simple option descriptions.
-type SimpleOptions = [SimpleOptionDescr]
-
-type SimpleOptionDescr = Opt.OptDescr SimpleOption
-
+-- | Simple option.
 data SimpleOption' a
     = SimpleFlag a
     | SimpleOpt  a (Maybe String)
     | SimpleReq  a String
       deriving (Show, Eq, Ord)
 
+-- | Option definition.
+type SimpleOptionDescr = Opt.OptDescr SimpleOption
+
+-- | Set of option definitions.
+type SimpleOptions = [SimpleOptionDescr]
+
 
 -- --------------------------------------------  Option type
 
--- | Describe option for flag (switch) type.
+-- | Option definition for flag (switch) type.
+--
+--   > flag ['f'] ["foo"] "Echo foo"
+--
 flag :: [ShortOption] -> [LongOption] -> String -> SimpleOptionDescr
 flag short long = Opt.Option short long $ Opt.NoArg simple where
     simple = SimpleFlag $ head long
 
--- | Describe option with optional one parameter.
+-- | Option definition with optional one parameter.
+--
+--   > opt [] ["bar"] "text" "Echo bar or text"
+--
 opt :: [ShortOption] -> [LongOption] -> String -> String -> SimpleOptionDescr
 opt short long name = Opt.Option short long (Opt.OptArg simple name) where
     simple = SimpleOpt $ head long
 
--- | Describe option with required one parameter.
+-- | Option definition with required one parameter.
+--
+--   > req [] ["baz"] "text" "Echo baz and text"
+--
 req :: [ShortOption] -> [LongOption] -> String -> String -> SimpleOptionDescr
 req short long name = Opt.Option short long (Opt.ReqArg simple name) where
     simple = SimpleReq $ head long
 
--- | Get flag of flag-type option.
+-- | Get flag of 'flag'-type option.
 getFlag :: [SimpleOption] -> LongOption -> Bool
 getFlag opts name = or $ map (getFlag1 name) opts
 
--- | Get parameter list of opt-type option.
+-- | Get parameter list of 'opt'-type option.
 getOpt :: [SimpleOption] -> LongOption -> [String]
 getOpt opts name = getOpt1 name `Maybe.mapMaybe` opts
 
--- | Get parameter list of req-type option.
+-- | Get parameter list of 'req'-type option.
 getReq :: [SimpleOption] -> LongOption -> [String]
 getReq opts name = getReq1 name `Maybe.mapMaybe` opts
 
@@ -96,17 +108,6 @@ getReq1 name (SimpleReq n arg) | (n == name) = Just arg
 getReq1 _ _ = Nothing
 
 
--- --------------------------------------------  Predefined option
-
--- | Predefined @-h@ or @--help@ option.
-help :: SimpleOptionDescr
-help = flag "h" ["help"] "Show help message"
-
--- | Predefined @-V@ or @--version@ option.
-version :: SimpleOptionDescr
-version = flag "V" ["version"] "Show version number"
-
-
 -- --------------------------------------------  Parsing
 
 -- | Result of parsing command-line parameters, consist of
@@ -117,6 +118,16 @@ type ParseResult' opt = Either [String] ([opt], [String])
 type ParseResult = ParseResult' SimpleOption
 
 -- | Parse parameters.
+--
+--   >>> parse [help, version] ["-h", "foo", "bar"]
+--   Right ([SimpleFlag "help"],["foo","bar"])
+--
+--   >>> parse [opt [] ["a"] "text" "optional text"] ["--a", "--a=foo", "bar"]
+--   Right ([SimpleOpt "a" Nothing, SimpleOpt "a" (Just "foo")], ["bar"])
+--
+--   >>> parse [req [] ["a"] "text" "required text"] ["--a=foo", "bar"]
+--   Right ([SimpleReq "a" "foo"], ["bar"])
+--
 parse :: SimpleOptions -> [String] -> ParseResult
 parse opts args = case Opt.getOpt Opt.Permute opts args of
                (opts', args', err) | null err  -> Right (opts', args')
@@ -127,12 +138,28 @@ parseCommand :: SimpleOptions -> IO ParseResult
 parseCommand opts = do args <- Env.getArgs
                        return $ parse opts args
 
--- | Print help message.
-printHelp :: [String] -> SimpleOptions -> IO ()
-printHelp text opts = do putStr $ helpMessage text opts
-                         putStrLn ""
+
+-- --------------------------------------------  Predefined option
+
+-- | Predefined @-h@ or @--help@ option.
+--
+--   > flag "h" ["help"] "Show help message"
+--
+help :: SimpleOptionDescr
+help = flag "h" ["help"] "Show help message"
 
 -- | Create help message.
 helpMessage :: [String] -> SimpleOptions -> String
 helpMessage text = Opt.usageInfo (unlines text ++ "OPTION")
 
+-- | Print help message.
+printHelp :: [String] -> SimpleOptions -> IO ()
+printHelp text opts = do putStr $ helpMessage text opts
+                         putStrLn ""
+
+-- | Predefined @-V@ or @--version@ option.
+--
+--   > flag "V" ["version"] "Show version number"
+--
+version :: SimpleOptionDescr
+version = flag "V" ["version"] "Show version number"
