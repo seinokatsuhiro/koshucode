d1d417b62cd949332287794f2e840f0b21ed73a8
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-10 11:25:20 +0900

    custom-digit encoder for integers

--- baala/overture/Koshucode/Baala/Overture/Text/Integer.hs
+++ baala/overture/Koshucode/Baala/Overture/Text/Integer.hs
@@ -8,9 +8,11 @@ module Koshucode.Baala.Overture.Text.Integer
     stringHex, stringHexInt, stringHexInteger,
     -- * Encoder
     intLowerHexString, intUpperHexString,
+    integralCustomString, integralReckonString,
   ) where
 
 import qualified Data.Char                      as Ch
+import qualified Data.IntMap.Strict             as Map
 import qualified Numeric                        as Num
 
 
@@ -91,3 +93,54 @@ intLowerHexString = integralLowerHexString
 --
 intUpperHexString :: Int -> String
 intUpperHexString = integralUpperHexString
+
+-- | Encode integer to custome-digit string.
+--
+--   >>> integralCustomString "OI" <$> [0..8 :: Int]
+--   ["O", "I", "IO", "II", "IOO", "IOI", "IIO", "III", "IOOO"]
+--
+integralCustomString :: (Integral n) => String -> n -> String
+integralCustomString digits = loop "" . abs where
+    dig  = Map.fromList $ zip [0..] digits
+    base = integralLength digits
+    loop s 0 | null s    = [dig Map.! 0]
+             | otherwise = s
+    loop s n = case quotRem n base of
+                 (n', i) -> loop (dig ! i : s) n'
+
+-- | Encode integer to reckoning string.
+--
+--   >>> intReckonString ['A'..'Z'] <$> [0, 1, 2, 25, 26, 27, 52, 53, 100, 1000, 5303449]
+--   ["", "A", "B", "Y", "Z", "AA", "AZ", "BA", "CV", "ALL", "KOSHU"]
+--
+integralReckonString :: (Integral n) => String -> n -> String
+integralReckonString digits = loop "" . abs where
+    dig       = Map.fromList $ zip [0..] $ '_' : digits
+    base      = integralLength digits
+    loop s 0  = s
+    loop s n  = case reckonDiv n base of
+                  (n', m) -> loop (dig ! m : s) n'
+
+-- | Integral version
+(!) :: (Integral n) => Map.IntMap a -> n -> a
+(!) m n = m Map.! fromIntegral n
+
+-- | Integral version of 'length'.
+--
+--   >>> integralLength "abc" :: Integer
+--   3
+--
+integralLength :: (Integral n) => [a] -> n
+integralLength [] = 0
+integralLength (_ : xs) = 1 + integralLength xs
+
+-- | Reckoning division.
+--
+--  >>> (`reckonDiv` 3) <$> [1 .. 10 :: Int]
+--  [(0,1),(0,2),(0,3),(1,1),(1,2),(1,3),(2,1),(2,2),(2,3),(3,1)]
+--
+reckonDiv :: (Integral n) => n -> n -> (n, n)
+reckonDiv x y =
+    case quotRem x y of
+      (q, r) | r == 0    -> (q - 1, y)
+             | otherwise -> (q, r)
