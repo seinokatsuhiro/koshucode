de0b9a44195d3f22269a2300d2f1519ece63e648
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-10 16:11:06 +0900

    create judge module directory

---
+++ baala/data/Koshucode/Baala/Data/Type/Judge.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Judgement and related types.
+
+module Koshucode.Baala.Data.Type.Judge
+  ( module Koshucode.Baala.Data.Type.Judge.Interp,
+    module Koshucode.Baala.Data.Type.Judge.Judge,
+    module Koshucode.Baala.Data.Type.Judge.JudgeClass,
+  ) where
+
+import Koshucode.Baala.Data.Type.Judge.Interp
+import Koshucode.Baala.Data.Type.Judge.Judge
+import Koshucode.Baala.Data.Type.Judge.JudgeClass
+
