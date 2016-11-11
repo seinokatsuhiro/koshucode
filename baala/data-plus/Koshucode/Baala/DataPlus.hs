95d84136825dd935414a8101d48726e462b0f009
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-11 14:54:57 +0900

    create data-plus package

---
+++ baala/data-plus/Koshucode/Baala/DataPlus.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Bundle of overture, base, syntax, and data packages.
+
+module Koshucode.Baala.DataPlus
+  ( module Koshucode.Baala.Overture,
+    module Koshucode.Baala.Base,
+    module Koshucode.Baala.Syntax,
+    module Koshucode.Baala.Data,
+  ) where
+
+import Koshucode.Baala.Overture
+import Koshucode.Baala.Base
+import Koshucode.Baala.Syntax
+import Koshucode.Baala.Data
