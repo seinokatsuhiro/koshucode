0cd159d0edcad4670ce81a916b8321bb4bf7caad
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-13 05:48:05 +0900

    move content mapper in class directory

--- baala/data/Koshucode/Baala/Data/Class.hs
+++ baala/data/Koshucode/Baala/Data/Class.hs
@@ -5,12 +5,14 @@
 module Koshucode.Baala.Data.Class
   ( module Koshucode.Baala.Data.Class.Complex,
     module Koshucode.Baala.Data.Class.Content,
+    module Koshucode.Baala.Data.Class.Map,
     module Koshucode.Baala.Data.Class.Simple,
     module Koshucode.Baala.Data.Class.Singleton,
   ) where
 
 import Koshucode.Baala.Data.Class.Complex
 import Koshucode.Baala.Data.Class.Content
+import Koshucode.Baala.Data.Class.Map
 import Koshucode.Baala.Data.Class.Simple
 import Koshucode.Baala.Data.Class.Singleton
 
--- baala/data/Koshucode/Baala/Data/Content/Utility.hs
+++ baala/data/Koshucode/Baala/Data/Content/Utility.hs
@@ -1,81 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
 
--- | Utilities for term contents.
+-- | Content encoder.
 
 module Koshucode.Baala.Data.Content.Utility
-  ( -- * Get & Put
-    CGetPut,
-    gpText, gpList, gpSet, gpSetSort,
-
-    -- * Conversion
-    contentAp, contentMap,
-    contentApText,
-    contentMapTextList,
-
-    -- * Encode
+  ( -- * Encode
     contentString,
     mixBracketList,
     mixBracketSet,
   ) where
 
-import qualified Koshucode.Baala.Overture                 as O
 import qualified Koshucode.Baala.Base                     as B
 import qualified Koshucode.Baala.Syntax                   as S
 import qualified Koshucode.Baala.Data.Type                as D
 import qualified Koshucode.Baala.Data.Class               as D
-
-
--- ----------------------  Get & Put
-
--- | Pair of get and put.
-type CGetPut a c = (c -> a, a -> c)
-
--- | 'gText' and 'pText'.
-gpText :: (D.CText c) => CGetPut String c
-gpText = (D.gText, D.pText)
-
--- | 'gList' and 'pList'.
-gpList :: (D.CList c) => CGetPut [c] c
-gpList = (D.gList, D.pList)
-
--- | 'gSet' and 'pSet'.
-gpSet :: (D.CSet c) => CGetPut [c] c
-gpSet = (D.gSet, D.pSet)
-
--- | 'gSetSort' and 'pSet'.
-gpSetSort :: (Ord c, D.CSet c) => CGetPut [c] c
-gpSetSort = (D.gSetSort, D.pSet)
-
-
--- ----------------------  Conversion
-
--- | Apply function to internal value of content.
---
---   >>> contentAp gText pText reverse (the $ pText "abc") :: BaalaC
---   VText "cba"
---
-contentAp :: (c -> a) -> (b -> c') -> (a -> b) -> c -> c'
-contentAp get put f = put . f . get
-
--- | Map function to internal value of content.
-contentMap :: (Functor f) => (c -> f a) -> (f b -> c') -> (a -> b) -> c -> c'
-contentMap get put f = contentAp get put $ fmap f
-
--- | Apply function to internal string of text content.
---
---   >>> contentApText reverse (the $ pText "abc")
---   VText "cba"
---
-contentApText :: (D.CText c) => O.StringMap -> O.Map c
-contentApText = contentAp D.gText D.pText
-
--- | Map function to characters of internal string of text content.
---
---   >>> contentMapTextList (\c -> pText [c]) (the $ pText "abc")
---   VList [VText "a",VText "b",VText "c"]
---
-contentMapTextList :: (D.CList c, D.CText c) => (Char -> c) -> O.Map c
-contentMapTextList = contentMap D.gText D.pList
 
 
 -- ----------------------  Encode
