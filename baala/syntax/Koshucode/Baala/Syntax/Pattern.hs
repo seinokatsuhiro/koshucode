fcc9e412501d44ba987b2482e0570ecfbc8de802
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-11-27 10:27:56 +0900

    create pattern bundle module

---
+++ baala/syntax/Koshucode/Baala/Syntax/Pattern.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Syntax pattern.
+
+module Koshucode.Baala.Syntax.Pattern
+  ( module Koshucode.Baala.Syntax.Token.Pattern,
+    module Koshucode.Baala.Syntax.TTree.Pattern,
+  ) where
+
+import Koshucode.Baala.Syntax.Token.Pattern
+import Koshucode.Baala.Syntax.TTree.Pattern
