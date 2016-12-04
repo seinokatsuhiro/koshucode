d17ed9db46d6ea85243fd7718a6f57690cec2bdd
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-05 06:54:41 +0900

    integer list

--- baala/overture/Koshucode/Baala/Overture/Misc.hs
+++ baala/overture/Koshucode/Baala/Overture/Misc.hs
@@ -3,8 +3,17 @@
 -- | Miscellaneous functions.
 
 module Koshucode.Baala.Overture.Misc
- ( printList
+ ( ints,
+   printList,
  ) where
+
+-- | Inteeger list start with given integer.
+--
+--   >>> take 4 $ ints 1
+--   [1,2,3,4]
+--
+ints :: Int -> [Int]
+ints n = [n ..]
 
 -- | Print list.
 --
