36e007efc741c437e3b17522905abe4bfc7d8dce
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-07 18:29:25 +0900

    create read module

--- baala/data/Koshucode/Baala/Data/Decode.hs
+++ baala/data/Koshucode/Baala/Data/Decode.hs
@@ -5,11 +5,13 @@
 module Koshucode.Baala.Data.Decode
   ( module Koshucode.Baala.Data.Decode.Content,
     module Koshucode.Baala.Data.Decode.Numeric,
+    module Koshucode.Baala.Data.Decode.Read,
     module Koshucode.Baala.Data.Decode.Term,
     module Koshucode.Baala.Data.Decode.Type,
   ) where
 
 import Koshucode.Baala.Data.Decode.Content
 import Koshucode.Baala.Data.Decode.Numeric
+import Koshucode.Baala.Data.Decode.Read
 import Koshucode.Baala.Data.Decode.Term
 import Koshucode.Baala.Data.Decode.Type
---
+++ baala/data/Koshucode/Baala/Data/Decode/Read.hs
@@ -0,0 +1,38 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Read data file.
+
+module Koshucode.Baala.Data.Decode.Read
+  ( readDataset,
+  ) where
+
+import qualified Koshucode.Baala.Base                    as B
+import qualified Koshucode.Baala.Syntax                  as S
+import qualified Koshucode.Baala.Data.Type               as D
+import qualified Koshucode.Baala.Data.Class              as D
+import qualified Koshucode.Baala.Data.Decode.Term        as D
+import qualified Koshucode.Baala.Data.Decode.Content     as D
+import qualified Koshucode.Baala.Syntax.Pattern          as P
+
+-- | Read file and convert to dataset.
+--
+--   >>> readDataset "foo.k" :: Koshucode.Baala.Base.IOAb DatasetC
+--   Right Dataset { A * 3 | B * 2 | C * 1 }
+--
+readDataset :: (D.CContent c) => FilePath -> B.IOAb (D.Dataset c)
+readDataset path =
+    do ts' <- S.readClauseTrees path
+       case ts' of
+         Left a   -> return $ Left a
+         Right ts -> case clauseJudges ts of
+                       Left a   -> return $ Left a
+                       Right js -> return $ Right $ D.dataset js
+
+clauseJudges :: (D.CContent c) => [[S.TTree]] -> B.Ab [D.Judge c]
+clauseJudges = loop D.cacheT [] where
+    loop cc js ((P.L (P.TBar "|--") : P.LRaw cl : ts) : ls) =
+        do (cc', j) <- D.treesJudge cc D.AssertAffirm cl ts
+           loop cc' (j : js) ls
+    loop _ js [] = Right js
+    loop _ _ ls  = B.abortable "clause" ls $ Left $ B.abortBecause "Except judgement"
+
