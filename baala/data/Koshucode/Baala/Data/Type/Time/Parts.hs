a851265f1bb8e0f88c0a87aa6ef9d9184d268422
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-11 01:33:49 +0900

    end of month and nth day of week

--- baala/data/Koshucode/Baala/Data/Type/Time/Parts.hs
+++ baala/data/Koshucode/Baala/Data/Type/Time/Parts.hs
@@ -12,7 +12,7 @@ module Koshucode.Baala.Data.Type.Time.Parts
     -- * Date parts
     Year, Month, Week, Day,
     DateParts (..),
-    dateYmd, dateYwd, dateYd,
+    dateYmd, dateYwd, dateYd, dateEom, dateYmnd,
 
     -- * Clock parts
     Days, Hour, Min, Sec,
@@ -85,10 +85,12 @@ type Day = Int
 
 -- | Decomposed date.
 data DateParts
-    = DateYmd Year Month Day  -- ^ Monthly date
-    | DateYwd Year Week Day   -- ^ Weekly date
-    | DateYd  Year Day        -- ^ Yearly date
-    | DateMjd Mjd             -- ^ MJD date
+    = DateMjd  Mjd                 -- ^ MJD date
+    | DateYmd  Year Month Day      -- ^ Monthly date
+    | DateYwd  Year Week Day       -- ^ Weekly date
+    | DateYd   Year Day            -- ^ Yearly date
+    | DateEom  Year Month          -- ^ End of month
+    | DateYmnd Year Month Int Day  -- ^ /N/\'th day of week in a month
       deriving (Show, Eq, Ord)
 
 -- | Monthly date parts.
@@ -103,20 +105,59 @@ dateYwd = DateYwd
 dateYd :: Year -> Day -> DateParts
 dateYd = DateYd
 
-instance ToMjdClip DateParts where
-    toMjdClip (DateYmd y m d)  = Tim.fromGregorian   y m d
-    toMjdClip (DateYwd y w d)  = Tim.fromWeekDate    y w d
-    toMjdClip (DateYd  y d)    = Tim.fromOrdinalDate y d
-    toMjdClip (DateMjd mjd)    = mjd
+-- | End of month.
+dateEom :: Year -> Month -> DateParts
+dateEom = DateEom
 
-    toMjdAb (DateYmd y m d)  = abMjd (Msg.notMonthlyDate y m d) $ Tim.fromGregorianValid   y m d
-    toMjdAb (DateYwd y w d)  = abMjd (Msg.notWeeklyDate y w d)  $ Tim.fromWeekDateValid    y w d
-    toMjdAb (DateYd  y d)    = abMjd (Msg.notYearlyDate y d)    $ Tim.fromOrdinalDateValid y d
-    toMjdAb (DateMjd mjd)    = Right mjd
+-- | /N/\'th day of week in a month
+--
+--   >>> toMjdClip (dateYmnd 2013 4 3 4)
+--   2013-04-18
+--
+dateYmnd :: Year -> Month -> Int -> Day -> DateParts
+dateYmnd = DateYmnd
+
+instance ToMjdClip DateParts where
+    toMjdClip (DateMjd mjd)       = mjd
+    toMjdClip (DateYmd  y m d)    = Tim.fromGregorian   y m d
+    toMjdClip (DateYwd  y w d)    = Tim.fromWeekDate    y w d
+    toMjdClip (DateYd   y d)      = Tim.fromOrdinalDate y d
+    toMjdClip (DateEom  y m)      = Tim.fromGregorian   y m 31
+    toMjdClip (DateYmnd y m n d)  = toMjdClip $ monthlyYmnd y m n d
+
+    toMjdAb (DateMjd   mjd)      = Right mjd
+    toMjdAb (DateYmd   y m d)    = abMjd (Msg.notMonthlyDate y m d) $ Tim.fromGregorianValid   y m d
+    toMjdAb (DateYwd   y w d)    = abMjd (Msg.notWeeklyDate  y w d) $ Tim.fromWeekDateValid    y w d
+    toMjdAb (DateYd    y d)      = abMjd (Msg.notYearlyDate  y d)   $ Tim.fromOrdinalDateValid y d
+    toMjdAb d@(DateEom _ _)      = Right $ toMjdClip d
+    toMjdAb (DateYmnd  y m n d)  = toMjdAb $ monthlyYmnd y m n d
 
 abMjd :: B.Ab Mjd -> Maybe Mjd -> B.Ab Mjd
 abMjd _ (Just mjd) = Right mjd
 abMjd a (Nothing)  = a
+
+monthlyYmnd :: Year -> Month -> Int -> Day -> DateParts
+monthlyYmnd y m n d = let diff = diffDayW (bomDayW y m) d
+                      in DateYmd y m (diff + 1 + 7 * (n - 1))
+
+-- | Day of week of the beginning of month.
+bomDayW :: Year -> Month -> Day
+bomDayW y m = d where
+    mjd = Tim.fromGregorian y m 1
+    (_, _, d) = Tim.toWeekDate mjd
+
+-- | Interval of weekday to next weekday.
+--
+--   >>> diffDayW (fromEnum Wednesday) (fromEnum Saturday)
+--   3
+--
+--   >>> diffDayW (fromEnum Saturday) (fromEnum Wednesday)
+--   4
+--
+diffDayW :: Day -> Day -> Int
+diffDayW from to
+    | from > to  = to + 7 - from
+    | otherwise  = to - from
 
 
 -- ---------------------------------  Clock
