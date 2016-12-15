a8bec41d788f8e40fc3f753b05919ce8a10afc65
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-16 03:23:54 +0900

    add type package to data-plus

--- baala/data-plus/Koshucode/Baala/DataPlus.hs
+++ baala/data-plus/Koshucode/Baala/DataPlus.hs
@@ -1,15 +1,17 @@
 {-# OPTIONS_GHC -Wall #-}
 
--- | Bundle of overture, base, syntax, and data packages.
+-- | Bundle of overture, base, syntax, type, and data packages.
 
 module Koshucode.Baala.DataPlus
   ( module Koshucode.Baala.Overture,
     module Koshucode.Baala.Base,
     module Koshucode.Baala.Syntax,
+    module Koshucode.Baala.Type,
     module Koshucode.Baala.Data,
   ) where
 
 import Koshucode.Baala.Overture
 import Koshucode.Baala.Base
 import Koshucode.Baala.Syntax
+import Koshucode.Baala.Type
 import Koshucode.Baala.Data
