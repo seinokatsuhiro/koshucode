ba717a6a3bbea1d44104c82a1778be76bb2ffcd1
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-20 15:30:19 +0900

    remove ad-lib tag

--- baala/base/Koshucode/Baala/Base/Abort/Message.hs
+++ baala/base/Koshucode/Baala/Base/Abort/Message.hs
@@ -14,9 +14,9 @@ import qualified Koshucode.Baala.Base.Abort    as B
 bug :: String -> B.Ab a
 bug reason = Left $ B.abortBecause $ "BUG: " ++ reason
 
--- | AD-LIB: /reason/
+-- | /reason/
 adlib :: String -> B.Ab a
-adlib reason = Left $ B.abortBecause $ "AD-LIB: " ++ reason
+adlib reason = Left $ B.abortBecause reason
 
 -- | Unsupported feature.
 unsupported :: String -> B.Ab a
