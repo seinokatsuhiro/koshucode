b1e8aab90690cf1d228371e872f53dea8b436bff
SEINO Katsuhiro <seinokatsuhiro@me.com>
2016-12-31 01:02:16 +0900

    indent branch

--- baala/base/Koshucode/Baala/Base/Code/Bracket.hs
+++ baala/base/Koshucode/Baala/Base/Code/Bracket.hs
@@ -9,6 +9,9 @@ module Koshucode.Baala.Base.Code.Bracket
     Bracket (..),
     GetBracketType,
     bracketTable,
+
+    -- * Indent
+    indentBranch,
   ) where
 
 import qualified Koshucode.Baala.Overture              as O
@@ -64,3 +67,50 @@ bracketTable xs = bracketType where
           Just p  -> p
           Nothing -> BracketNone
 
+
+-- ============================================  Indent
+
+-- | Indent branching function.
+--   This inserts open\/separator\/close elements by following indent.
+--
+--   >>> let size s = if dropWhile (== '/') s == "" then Just (length s) else Nothing
+--   >>> let test s = s `elem` [">", "-"]
+--   >>> let toks s = takeWhile (== '/') s : words (dropWhile (== '/') s)
+--   >>> let text = Right . unwords . concat
+--   >>> let conv ls = text =<< indentBranch size test "(" "|" ")" (toks <$> ls)
+--
+--   >>> toks "/> bb"
+--   ["/",">","bb"]
+--
+--   >>> conv ["> aa", "/> bb", "//> cc"]
+--   Right "> aa ( > bb ( > cc ) )"
+--
+--   >>> conv ["> aa", "/> bb", "/> cc"]
+--   Right "> aa ( > bb | > cc )"
+--
+--   >>> conv ["> aa", "/> bb", "//- cc", "///cc", "//- dd", "//- ee", "/- ff"]
+--   Right "> aa ( > bb ( - cc cc | - dd | - ee ) | - ff )"
+--
+indentBranch
+    :: (a -> Maybe Int)  -- ^ Function getting indent size
+    -> O.Test a          -- ^ Testing indent element
+    -> a                 -- ^ Open element
+    -> a                 -- ^ Separator element
+    -> a                 -- ^ Close element
+    -> [[a]]             -- ^ Code elements indented
+    -> B.Ab [[a]]        -- ^ Code Elements with open\/separator\/close elements
+indentBranch size indent open sep close = line 0 0 where
+    line lv i (((size -> Just i') : xs@(k : _)) : ls)
+         | not $ indent k = next lv       i           xs  ls
+         | i' == 0        = next lv       i           xs  ls
+         | i' >  i        = next (lv + 1) i' (open  : xs) ls
+         | i' <  i        = next (lv - 1) i' (close : sep : xs) ls
+         | i' == i        = next lv       i' (sep   : xs) ls
+    line lv i (xs : ls)   = next lv i xs ls
+    line lv _ []
+        | lv == 0         = Right []
+        | lv >  0         = Right [replicate lv close]
+        | otherwise       = Msg.extraCloseBracketInserted
+
+    next lv' i' xs' ls    = do ls' <- line lv' i' ls
+                               Right $ xs' : ls'
