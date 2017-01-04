86e0acad8d09b334b6dcbb89982915b93f187cf6
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-01-04 11:34:08 +0900

    reexport subtree module

--- baala/syntax/Koshucode/Baala/Syntax.hs
+++ baala/syntax/Koshucode/Baala/Syntax.hs
@@ -5,6 +5,7 @@
 module Koshucode.Baala.Syntax
   ( module Koshucode.Baala.Syntax.Attr,
     module Koshucode.Baala.Syntax.Para,
+    module Koshucode.Baala.Syntax.Subtree,
     module Koshucode.Baala.Syntax.Symbol,
     module Koshucode.Baala.Syntax.Token,
     module Koshucode.Baala.Syntax.Tree,
@@ -12,6 +13,7 @@ module Koshucode.Baala.Syntax
 
 import Koshucode.Baala.Syntax.Attr
 import Koshucode.Baala.Syntax.Para
+import Koshucode.Baala.Syntax.Subtree
 import Koshucode.Baala.Syntax.Symbol
 import Koshucode.Baala.Syntax.Token
 import Koshucode.Baala.Syntax.Tree
