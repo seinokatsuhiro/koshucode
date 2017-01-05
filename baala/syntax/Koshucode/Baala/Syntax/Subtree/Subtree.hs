c56f0fedf82f760f30314622f3ef82b5159b07cb
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-01-05 09:16:07 +0900

    subtree with string getter

--- baala/syntax/Koshucode/Baala/Syntax/Subtree/Subtree.hs
+++ baala/syntax/Koshucode/Baala/Syntax/Subtree/Subtree.hs
@@ -4,13 +4,16 @@
 
 module Koshucode.Baala.Syntax.Subtree.Subtree
   ( -- * Pattern
-    Subtree,
-    SubtreeOutput,
     SubtreePattern (..),
     subtreeL,
     SubtreeTerm (..),
     subtreeText, subtreeTexts,
-    subtree, subtreeOne,
+
+    -- * Conversion
+    Subtree,
+    SubtreeOutput,
+    subtree, subtreeWith,
+    subtreeOne,
   ) where
 
 import qualified Koshucode.Baala.Overture                as O
@@ -18,11 +21,8 @@ import qualified Koshucode.Baala.Base                    as B
 import qualified Koshucode.Baala.Syntax.Symbol           as S
 import qualified Koshucode.Baala.Syntax.Subtree.Filter   as S
 
--- | Subtree.
-type Subtree a = B.RawTree [SubtreePattern] a a
 
--- | Subtree output.
-type SubtreeOutput a = B.RawTree [SubtreePattern] a (SubtreeTerm, a)
+-- ============================================  Pattern
 
 -- | Subtree pattern.
 data SubtreePattern
@@ -31,11 +31,11 @@ data SubtreePattern
     | SubtreeR S.SubtreeFilter [SubtreePattern]  -- ^ Recursive branch
       deriving (Show, Eq)
 
--- | Create non-termination leaf pattern.
+-- | Create non-termification leaf pattern.
 subtreeL :: S.SubtreeFilter -> SubtreePattern
 subtreeL = SubtreeL SubtreeNone
 
--- | Termination of subtree content.
+-- | Termification of subtree content.
 data SubtreeTerm
     = SubtreeNone
     | SubtreeText [String] S.TermName
@@ -50,49 +50,63 @@ subtreeText c = subtreeTexts [c]
 subtreeTexts :: (S.ToTermName n) => [String] -> n -> SubtreeTerm
 subtreeTexts cs n = SubtreeText cs $ S.toTermName n
 
+
+-- ============================================  Conversion
+
+-- | Subtree.
+type Subtree a = B.RawTree [SubtreePattern] a a
+
+-- | Subtree with termified elements.
+type SubtreeOutput a = B.RawTree [SubtreePattern] a (SubtreeTerm, a)
+
 -- | Select subtree.
 --
 --   >>> let tree = B.TreeB [] "Y1" [B.TreeL "Z1", B.TreeL "Z2", B.TreeB [] "Y2" [B.TreeL "Z3"]]
---   >>> O.putLines $ B.ppRawTree tree
+--   >>> B.printTree tree
 --   > [] "Y1"
 --     - "Z1"
 --     - "Z2"
 --     > [] "Y2"
 --       - "Z3"
 --
---   >>> O.putLines (B.ppRawTree O.<++> subtree [SubtreeB (subtreeEq "Y1") [subtreeL (subtreeEq "Z1")]] [tree])
+--   >>> B.printTrees $ subtree [SubtreeB (S.subtreeEq "Y1") [subtreeL (S.subtreeEq "Z1")]] [tree]
 --   > [] "Y1"
 --     - (SubtreeNone,"Z1")
 --
---   >>> O.putLines (B.ppRawTree O.<++> subtree [SubtreeR (subtreeId) [subtreeL (subtreeEq "Z3")]] [tree])
+--   >>> B.printTrees $ subtree [SubtreeR (S.subtreeId) [subtreeL (S.subtreeEq "Z3")]] [tree]
 --   > [] "Y1"
 --     > [] "Y2"
 --       - (SubtreeNone,"Z3")
 --
---   >>> O.putLines (B.ppRawTree O.<++> subtree [SubtreeR (subtreeId) [subtreeL (subtreeEq "Z1")]] [tree])
+--   >>> B.printTrees $ subtree [SubtreeR (S.subtreeId) [subtreeL (S.subtreeEq "Z1")]] [tree]
 --   > [] "Y1"
 --     - (SubtreeNone,"Z1")
 --
---   >>> O.putLines (B.ppRawTree O.<++> subtree [SubtreeB (subtreeId) [SubtreeL (subtreeText "A" "/z") (subtreeEq "Z1")]] [tree])
+--   >>> B.printTrees $ subtree [SubtreeB (S.subtreeId) [SubtreeL (subtreeText "A" "/z") (S.subtreeEq "Z1")]] [tree]
 --   > [] "Y1"
 --     - (SubtreeText ["A"] (TermName EQ "z"),"Z1")
 --
 subtree :: [SubtreePattern] -> [Subtree String] -> [SubtreeOutput String]
-subtree ps0 ts = p1 O.<?> ts where
-    p1 t = maybeHead (p2 t O.<?> ps0)
+subtree = subtreeWith id
 
-    p2 t@(B.TreeL z) (SubtreeL term f)
-        | nullZ f t  = Nothing
-        | otherwise  = Just $ B.TreeL (term, z)
-    p2 t@(B.TreeB _ y xs) (SubtreeB f ps)
-        | nullY f t  = Nothing
-        | otherwise  = Just $ B.TreeB [] y (subtree ps xs)
-    p2 t@(B.TreeB _ y xs) (SubtreeR f ps)
-        | nullY f t  = Nothing
-        | otherwise  = case subtree (SubtreeR f ps : ps) xs of
-                         []  -> Nothing
-                         xs' -> Just $ B.TreeB [] y xs'
-    p2 _ _ = Nothing
+-- | Select subtree with string getter.
+subtreeWith :: (a -> String) -> [SubtreePattern] -> [Subtree a] -> [SubtreeOutput a]
+subtreeWith get = loop where
+    loop ps0 ts = p1 O.<?> ts where
+        p1 t = maybeHead (p2 t O.<?> ps0)
+
+        p2 t@(B.TreeL z) (SubtreeL term f)
+            | nullZ get f t  = Nothing
+            | otherwise      = Just $ B.TreeL (term, z)
+        p2 t@(B.TreeB _ y xs) (SubtreeB f ps)
+            | nullY get f t  = Nothing
+            | otherwise      = Just $ B.TreeB [] y (loop ps xs)
+        p2 t@(B.TreeB _ y xs) (SubtreeR f ps)
+            | nullY get f t  = Nothing
+            | otherwise      = case loop (SubtreeR f ps : ps) xs of
+                                 []  -> Nothing
+                                 xs' -> Just $ B.TreeB [] y xs'
+        p2 _ _ = Nothing
 
 -- | Select subtree.
 subtreeOne :: [SubtreePattern] -> O.Map [Subtree String]
@@ -100,25 +114,29 @@ subtreeOne ps0 ts = p1 O.<?> ts where
     p1 t = maybeHead (p2 t O.<?> ps0)
 
     p2 t@(B.TreeL _) (SubtreeL _ f)
-        | nullZ f t  = Nothing
-        | otherwise  = Just t
+        | nullZ id f t  = Nothing
+        | otherwise     = Just t
     p2 t@(B.TreeB _ y xs) (SubtreeB f ps)
-        | nullY f t  = Nothing
-        | otherwise  = Just $ B.TreeB ps y xs
+        | nullY id f t  = Nothing
+        | otherwise     = Just $ B.TreeB ps y xs
     p2 t@(B.TreeB _ y xs) (SubtreeR f ps)
-        | nullY f t  = Nothing
-        | otherwise  = Just $ B.TreeB (SubtreeR f ps : ps) y xs
+        | nullY id f t  = Nothing
+        | otherwise     = Just $ B.TreeB (SubtreeR f ps : ps) y xs
     p2 _ _ = Nothing
-
-nullZ :: S.SubtreeFilter -> Subtree String -> Bool
-nullZ f t = null $ S.subtreeFilterOn getTreeZ f [t]
-
-nullY :: S.SubtreeFilter -> Subtree String -> Bool
-nullY f t = null $ S.subtreeFilterOn getTreeY f [t]
 
 maybeHead :: [a] -> Maybe a
 maybeHead []       = Nothing
 maybeHead (a : _)  = Just a
+
+nullY :: (a -> String) -> S.SubtreeFilter -> Subtree a -> Bool
+nullY get f t = null $ S.subtreeFilterOn (get <.> getTreeY) f [t]
+
+nullZ :: (a -> String) -> S.SubtreeFilter -> Subtree a -> Bool
+nullZ get f t = null $ S.subtreeFilterOn (get <.> getTreeZ) f [t]
+
+-- | 'fmap' composition.
+(<.>) :: (Functor f) => (b -> c) -> (a -> f b) -> a -> f c
+(<.>) g f x = g <$> f x
 
 -- | Branch element of tree.
 getTreeY :: B.RawTree b y z -> Maybe y
