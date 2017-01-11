932cc65c6c3ab8139410d9e545bac9e5880cc972
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-01-12 07:48:30 +0900

    sieve module

--- baala/subtext/Koshucode/Baala/Subtext.hs
+++ baala/subtext/Koshucode/Baala/Subtext.hs
@@ -6,9 +6,11 @@ module Koshucode.Baala.Subtext
   ( module Koshucode.Baala.Subtext.Bundle,
     module Koshucode.Baala.Subtext.Match,
     module Koshucode.Baala.Subtext.Operator,
+    module Koshucode.Baala.Subtext.Sieve,
   ) where
 
 import Koshucode.Baala.Subtext.Bundle
 import Koshucode.Baala.Subtext.Match
 import Koshucode.Baala.Subtext.Operator
+import Koshucode.Baala.Subtext.Sieve
 
---
+++ baala/subtext/Koshucode/Baala/Subtext/Sieve.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- | Sieve pattern.
+
+module Koshucode.Baala.Subtext.Sieve
+ ( module Koshucode.Baala.Subtext.Sieve.Token,
+   module Koshucode.Baala.Subtext.Sieve.Tree,
+ ) where
+
+import Koshucode.Baala.Subtext.Sieve.Token
+import Koshucode.Baala.Subtext.Sieve.Tree
