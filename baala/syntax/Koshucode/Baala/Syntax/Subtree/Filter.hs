d6353da106da71e289e58d140867df8f9609c72f
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-01-17 06:24:24 +0900

    remove unnecessary functions

--- baala/syntax/Koshucode/Baala/Syntax/Subtree/Filter.hs
+++ baala/syntax/Koshucode/Baala/Syntax/Subtree/Filter.hs
@@ -8,8 +8,6 @@ module Koshucode.Baala.Syntax.Subtree.Filter
     subtreeKeep, subtreeOmit,
     subtreeChain,
     subtreeAttr,
-    subtreeFilter,
-    subtreeFilterOn,
   ) where
 
 import qualified Koshucode.Baala.Overture                as O
@@ -54,40 +52,3 @@ subtreeChain f g = SubtreeChain f g
 -- | Attribute filter.
 subtreeAttr :: String -> String -> SubtreeFilter
 subtreeAttr = SubtreeAttr
-
--- | Filter strings by subtree filter.
---
---   >>> subtreeFilter subtreeId ["foo", "bar", "foobar"]
---   ["foo","bar","foobar"]
---
---   >>> subtreeFilter (SubtreeEq "bar") ["foo", "bar", "foobar"]
---   ["bar"]
---
---   >>> subtreeFilter (subtreeKeep "foo*") ["foo", "bar", "foobar"]
---   ["foo","foobar"]
---
---   >>> subtreeFilter (subtreeKeep "*bar") ["foo", "bar", "foobar"]
---   ["bar","foobar"]
---
---   >>> subtreeFilter (subtreeOmit "foo*") ["foo", "bar", "foobar"]
---   ["bar"]
---
---   >>> subtreeFilter (subtreeOmit "foo*") ["foo", "bar", "foobar"]
---   ["bar"]
---
---   >>> subtreeFilter (subtreeKeep "foo*" O.++ subtreeOmit "*bar") ["foo", "bar", "foobar"]
---   ["foo"]
---
-subtreeFilter :: SubtreeFilter -> O.Map [String]
-subtreeFilter = subtreeFilterOn Just
-
-subtreeFilterOn :: (a -> Maybe String) -> SubtreeFilter -> O.Map [a]
-subtreeFilterOn get f xs0 = a xs0 [f] where
-    a xs []                        = xs
-    a xs (SubtreeId : fs)          = a xs fs
-    a xs (SubtreeEq x : fs)        = a xs' fs where xs' = O.keepOn get (== x) xs
-    a xs (SubtreeKeep _ e : fs)    = a xs' fs where xs' = O.keepOn get (S.sivMatchExpr e) xs
-    a xs (SubtreeOmit _ e : fs)    = a xs' fs where xs' = O.omitOn get (S.sivMatchExpr e) xs
-    a xs (SubtreeAttr _ _ : fs)    = a xs fs
-    a xs (SubtreeChain f1 f2 : fs) = a xs fs' where fs' = f1 : f2 : fs
-
