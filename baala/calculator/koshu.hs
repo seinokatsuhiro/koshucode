fcc1d5589bb6048b2f64f3aa2cb11552446956af
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-02-10 15:35:44 +0900

    main with command line arguments

--- baala/calculator/koshu.hs
+++ baala/calculator/koshu.hs
@@ -1,6 +1,9 @@
 #!/usr/bin/env runhaskell
 {-# OPTIONS_GHC -Wall #-}
 
+module Main where
+
+import qualified System.Environment                       as Env
 import qualified Koshucode.Baala.Base                     as B
 import qualified Koshucode.Baala.Core                     as C
 import qualified Koshucode.Baala.Toolkit.Library.Global   as Gl
@@ -9,6 +12,15 @@ import qualified Koshucode.Baala.Toolkit.Main.KoshuMain   as Main
 
 main :: IO ()
 main = B.exitWith =<< Main.koshuMain koshuGlobal
+
+-- The main function with command-line argument.
+-- To debug main function using GHCi:
+--
+-- > :l "koshu.hs"
+-- > :trace mainWith ["INPUT.k", ...]
+--
+mainWith :: [String] -> IO ()
+mainWith args = Env.withArgs args main
 
 koshuGlobal :: C.GlobalC
 koshuGlobal = Gl.baalaGlobal
