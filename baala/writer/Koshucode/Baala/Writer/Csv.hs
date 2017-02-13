9a4a75ae2c9b4c30767eec6b8123e7dcf48672e0
SEINO Katsuhiro <seinokatsuhiro@me.com>
2017-02-13 13:46:58 +0900

    csv output using mix text

--- baala/writer/Koshucode/Baala/Writer/Csv.hs
+++ baala/writer/Koshucode/Baala/Writer/Csv.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- | CSV output.
@@ -35,46 +37,47 @@ resultTsvHeading = C.ResultWriterChunk "tab-heading" $ hPutXsv tabHeadSetting
 hPutXsv :: forall c. (D.CContent c) => XsvSetting -> C.ResultWriterChunk c
 hPutXsv setting@XsvSetting { xsvHead = isHead, xsvSep = sep, xsvQuote = quote } h _ status sh =
     do let csv = concatMap toCsv chunks
-       B.hPutMixLines B.crlfBreak h $ map B.mix csv
+       B.hPutMixLines B.crlfBreak h csv
        return status
     where
       chunks = concatMap S.shortBody sh
 
-      toCsv :: C.ResultChunk c -> [String]
-      toCsv (C.ResultRel _ (T.Rel he bo)) = appendHead he $ map line bo
+      toCsv :: C.ResultChunk c -> [B.MixText]
+      toCsv (C.ResultRel _ (T.Rel he bo)) = appendHead he (line <$> bo)
       toCsv _ = []
 
-      join :: [String] -> String
-      join = B.intercalate sep
+      join :: [B.MixText] -> B.MixText
+      join = B.mixJoin $ B.mix sep
 
-      line :: [c] -> String
+      line :: [c] -> B.MixText
       line = join . map (csvContent setting)
 
       appendHead he xs
-          | isHead    = join ((quote . S.termNameContent) <$> T.getTermNames he) : xs
+          | isHead    = join (mixName <$> T.getTermNames he) : xs
           | otherwise = xs
+      mixName = (B.mix . quote . S.stringChars . S.termNameContent)
 
-csvContent :: (D.CContent c) => XsvSetting -> c -> String
+csvContent :: (D.CContent c) => XsvSetting -> c -> B.MixText
 csvContent setting@XsvSetting { xsvQuote = quote } c
-    | D.isCode   c  = quote $ O.tString $ D.gCode c
-    | D.isText   c  = quote $ O.tString $ D.gText c
-    | D.isTerm   c  = quote $ S.termNameString $ D.gTerm c
-    | D.isDec    c  = T.encodeDecimalCompact $ D.gDec c
-    | D.isClock  c  = quote $ B.mixToFlatString $ B.mixEncode $ D.gClock c
-    | D.isTime   c  = quote $ B.mixToFlatString $ B.mixEncode $ D.gTime c
-    | D.isBool   c  = boolToString $ D.gBool c
+    | D.isCode   c  = B.mix $ quote $ D.gCode c
+    | D.isText   c  = B.mix $ quote $ D.gText c
+    | D.isTerm   c  = B.mix $ quote $ S.termNameChars $ D.gTerm c
+    | D.isDec    c  = B.mix (T.encodeDecimalCompact $ D.gDec c :: S.Chars)
+    | D.isClock  c  = B.mixEncode $ D.gClock c
+    | D.isTime   c  = B.mixEncode $ D.gTime c
+    | D.isBool   c  = boolToMix $ D.gBool c
 
-    | D.isList   c  = csvSubContents setting $ D.gList c
-    | D.isSet    c  = csvSubContents setting $ D.gSet c 
-    | otherwise     = D.contentString c
+    | D.isList   c  = csvSubs setting $ D.gList c
+    | D.isSet    c  = csvSubs setting $ D.gSet c 
+    | otherwise     = B.mix $ D.contentString c
 
-csvSubContents :: (D.CContent c) => XsvSetting -> [c] -> String
-csvSubContents setting@XsvSetting { xsvSubSep = sub } cs =
-    B.intercalate sub $ csvContent setting <$> cs
+csvSubs :: (D.CContent c) => XsvSetting -> [c] -> B.MixText
+csvSubs setting@XsvSetting { xsvSubSep = sub } cs =
+    B.mixJoin (B.mix sub) (csvContent setting <$> cs)
 
-boolToString :: Bool -> String
-boolToString True  = "true"
-boolToString False = "false"
+boolToMix :: Bool -> B.MixText
+boolToMix True  = B.mix "true"
+boolToMix False = B.mix "false"
 
 -- --------------------------------------------  Setting
 
@@ -82,7 +85,7 @@ data XsvSetting = XsvSetting
     { xsvHead     :: Bool
     , xsvSep      :: String
     , xsvSubSep   :: String
-    , xsvQuote    :: O.StringMap
+    , xsvQuote    :: S.CharsMap
     }
 
 csvSetting :: XsvSetting
@@ -103,15 +106,33 @@ csvHeadSetting = csvSetting { xsvHead = True }
 tabHeadSetting :: XsvSetting
 tabHeadSetting = tabSetting { xsvHead = True }
 
-enquote :: O.StringMap
-enquote str = '"' : q str where
-    q ""          = ['"']
-    q ('"' : xs)  = '"' : '"' : q xs
-    q (x : xs)    =         x : q xs
+{-| Enclose text in double quotations.
 
-toSpace :: O.StringMap
-toSpace ""           = ""
-toSpace (x : xs) | Ch.isControl x    = ' ' : xs'
-                 | otherwise         =   x : xs'
-                 where xs' = toSpace xs
+    >>> putStrLn $ enquote "foo"
+    "foo"
+
+    >>> putStrLn $ enquote "foo\"bar"
+    "foo""bar"
+    -}
+enquote :: (O.Textual t) => t -> t
+enquote str = '"' O.<:> q str where
+    q (O.cut -> O.Jp x xs)
+        | x == '"'  = '"' O.<:> '"' O.<:> q xs
+        | otherwise =             x O.<:> q xs
+    q _ = O.charT '"'
+
+{-| Convert control characters to space character.
+
+    >>> putStrLn $ toSpace "foo"
+    foo
+
+    >>> putStrLn $ toSpace "foo\r\nbar"
+    foo  bar
+    -}
+toSpace :: (O.Textual t) => t -> t
+toSpace (O.cut -> O.Jp x xs)
+    | Ch.isControl x    = ' ' O.<:> xs'
+    | otherwise         =   x O.<:> xs'
+    where xs' = toSpace xs
+toSpace xs = xs
 
